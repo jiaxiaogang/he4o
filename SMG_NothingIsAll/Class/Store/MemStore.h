4c29fdd4917900c0d6cb3ec8bdb805dbb2b3f7f9
赤臂生 <jxg2764894@live.com>
2017-04-07 22:22:20 +0800

    MemStore记忆功能;

---
+++ SMG_NothingIsAll/Class/Store/MemStore.h
@@ -0,0 +1,18 @@
+//
+//  MemStore.h
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/4/7.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ *  MARK:--------------------记忆存储--------------------
+ *  1,MemStore.dic是内存kv存储;
+ *  2,MemStore的TMCache中有localKV存储;
+ */
+@interface MemStore : NSObject
+
+@end
---
+++ SMG_NothingIsAll/Class/Store/MemStore.m
@@ -0,0 +1,13 @@
+//
+//  MemStore.m
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/4/7.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import "MemStore.h"
+
+@implementation MemStore
+
+@end
