ba92cea02e46ef770734ce26bafc59ef3d6afd8c
赤臂生 <jxg2764894@live.com>
2017-04-08 10:56:00 +0800

    Happy

---
+++ SMG_NothingIsAll/Class/Mind/Happy/Happy.h
@@ -0,0 +1,13 @@
+//
+//  Happy.h
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/4/8.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Happy : NSObject
+
+@end
---
+++ SMG_NothingIsAll/Class/Mind/Happy/Happy.m
@@ -0,0 +1,13 @@
+//
+//  Happy.m
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/4/8.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import "Happy.h"
+
+@implementation Happy
+
+@end
