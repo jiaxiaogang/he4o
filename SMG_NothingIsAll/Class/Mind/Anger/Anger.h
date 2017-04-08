29a9415d13d5dfd0e4c86c71353eaa194698fa2c
赤臂生 <jxg2764894@live.com>
2017-04-08 10:55:07 +0800

    Anger

---
+++ SMG_NothingIsAll/Class/Mind/Anger/Anger.h
@@ -0,0 +1,13 @@
+//
+//  Anger.h
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/4/8.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Anger : NSObject
+
+@end
---
+++ SMG_NothingIsAll/Class/Mind/Anger/Anger.m
@@ -0,0 +1,13 @@
+//
+//  Anger.m
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/4/8.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import "Anger.h"
+
+@implementation Anger
+
+@end
