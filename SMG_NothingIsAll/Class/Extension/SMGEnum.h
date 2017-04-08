807695994f8e765ce2a6f25ffdd337d8c5aa5a32
赤臂生 <jxg2764894@live.com>
2017-04-08 13:50:16 +0800

    定义声音,图片,SMGEnum SMGHeader;

---
+++ SMG_NothingIsAll/Class/Extension/SMGEnum.h
@@ -0,0 +1,13 @@
+//
+//  SMGEnum.h
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/4/8.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#ifndef SMGEnum_h
+#define SMGEnum_h
+
+
+#endif /* SMGEnum_h */
---
+++ SMG_NothingIsAll/Class/Extension/SMGHeader.h
@@ -0,0 +1,9 @@
+//
+//  SMGHeader.h
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/4/8.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import "SMGEnum.h"
