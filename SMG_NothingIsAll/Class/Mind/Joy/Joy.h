d7ce15e37026165ae89801eddda74bdde01aca03
赤臂生 <jxg2764894@live.com>
2017-04-08 10:54:43 +0800

    Joy

---
+++ SMG_NothingIsAll/Class/Mind/Joy/Joy.h
@@ -0,0 +1,16 @@
+//
+//  Joy.h
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/4/8.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ *  MARK:--------------------喜--------------------
+ */
+@interface Joy : NSObject
+
+@end
---
+++ SMG_NothingIsAll/Class/Mind/Joy/Joy.m
@@ -0,0 +1,13 @@
+//
+//  Joy.m
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/4/8.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import "Joy.h"
+
+@implementation Joy
+
+@end
