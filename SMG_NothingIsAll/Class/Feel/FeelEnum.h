bfd2277e55e76c70a23a5526d3cb9e1652b1c39f
赤臂生 <jxg2764894@live.com>
2017-04-09 17:15:35 +0800

    FeelEnum

--- SMG_NothingIsAll/Class/Feel/FeelEnum.h
+++ SMG_NothingIsAll/Class/Feel/FeelEnum.h
@@ -1,15 +1,8 @@
 //
-//  Feel.h
+//  FeelEnum.h
 //  SMG_NothingIsAll
 //
 //  Created by 贾  on 2017/4/9.
 //  Copyright © 2017年 XiaoGang. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-
-@interface Feel : NSObject
-
-
-
-@end
