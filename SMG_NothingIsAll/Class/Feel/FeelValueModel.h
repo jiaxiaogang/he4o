ac08976ecf68e8c73ac20f64b9e4d47d8812928c
赤臂生 <jxg2764894@live.com>
2017-04-09 16:56:52 +0800

    create FeelValueModel

---
+++ SMG_NothingIsAll/Class/Feel/FeelValueModel.h
@@ -0,0 +1,17 @@
+//
+//  FeelValueModel.h
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/4/9.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+/**
+ *  MARK:--------------------感觉一项属性的值--------------------
+ */
+@interface FeelValueModel : NSObject
+
+@end
---
+++ SMG_NothingIsAll/Class/Feel/FeelValueModel.m
@@ -0,0 +1,13 @@
+//
+//  FeelValueModel.m
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/4/9.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import "FeelValueModel.h"
+
+@implementation FeelValueModel
+
+@end
