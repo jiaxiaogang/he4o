2e6fd16dc4e787a96833768287a458abe0d8fa81
赤臂生 <jxg2764894@live.com>
2017-04-14 10:06:44 +0800

    UI

--- SMG_NothingIsAll/Class/StudyViewController.m
+++ SMG_NothingIsAll/Class/StudyViewController.m
@@ -16,22 +16,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    [self initView];
+    [self  initData];
+    [self initDisplay];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void) initView{
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void) initData{
+    
 }
-*/
+
+-(void) initDisplay{
+    
+}
+
+-(void) viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self.navigationController setNavigationBarHidden:true animated:true];
+}
+
+
+
+
 
 @end
