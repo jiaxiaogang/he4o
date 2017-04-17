d5792182968d18d940cd220731449f4a647f6726
赤臂生 <jxg2764894@live.com>
2017-04-17 17:50:53 +0800

    finish UnderstandModel;

--- SMG_NothingIsAll/Class/Understand/UnderstandHeader.h
+++ SMG_NothingIsAll/Class/Understand/UnderstandHeader.h
@@ -7,3 +7,4 @@
 //
 
 #import "Understand.h"
+#import "UnderstandModel.h"
--- SMG_NothingIsAll/Class/Understand/UnderstandModel.h
+++ SMG_NothingIsAll/Class/Understand/UnderstandModel.h
@@ -8,6 +8,16 @@
 
 #import <Foundation/Foundation.h>
 
+
+/**
+ *  MARK:--------------------理解item模型--------------------
+ */
 @interface UnderstandModel : NSObject
+
+@property (strong,nonatomic) NSString *fromMKId;
+@property (strong,nonatomic) NSString *doType;
+@property (strong,nonatomic) NSString *toMKId;
+@property (strong,nonatomic) NSString *value;   //值(颜色为FFFFFF 高度为xxcm)
+
 
 @end
