a68438e77060243159843a0491af191eaff70496
赤臂生 <jxg2764894@live.com>
2017-04-20 23:18:37 +0800

    today finish;

--- SMG_NothingIsAll/Class/Store/Store.m
+++ SMG_NothingIsAll/Class/Store/Store.m
@@ -29,14 +29,22 @@
 /**
  *  MARK:--------------------搜索关于文字的记忆--------------------
  */
+//精确匹配
 -(NSDictionary*) searchMemStoreWithLanguageText:(NSString*)text{
     NSDictionary *where = [NSDictionary dictionaryWithObjectsAndKeys:STRTOOK(text),@"text", nil];
     return [self.memStore getSingleMemoryWithWhereDic:where];
 }
 
+//包含匹配
 -(NSMutableArray*) searchMemStoreContainerText:(NSString*)text limit:(NSInteger)limit{
     NSDictionary *where = [NSDictionary dictionaryWithObjectsAndKeys:STRTOOK(text), @"text",nil];
     return [self.memStore getMemoryContainsWhereDic:where limit:limit];//习惯池;从记忆中搜索数量很多的存到习惯中...//随后添加分词系统的作用使这里更厉害;
 }
+
+//包含且智能去重匹配(ABBC=ABBC只取一个;ABBCD=ABB只取一个;*?ab*..参考windows搜索的方式;
+//-(NSMutableArray*) searchMemStoreContainerText:(NSString*)text limit:(NSInteger)limit{
+//    NSDictionary *where = [NSDictionary dictionaryWithObjectsAndKeys:STRTOOK(text), @"text",nil];
+//    return [self.memStore getMemoryContainsWhereDic:where limit:limit];//习惯池;从记忆中搜索数量很多的存到习惯中...//随后添加分词系统的作用使这里更厉害;
+//}
 
 @end
