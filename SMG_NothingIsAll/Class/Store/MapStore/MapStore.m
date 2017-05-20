c452412a471123c2b109c63a2197afc90673ce7d
贾晓刚 <jxg2764894@live.com>
2017-05-20 21:28:13 +0800

    映射计数器++;

--- SMG_NothingIsAll/Class/Store/MapStore/MapStore.m
+++ SMG_NothingIsAll/Class/Store/MapStore/MapStore.m
@@ -13,6 +13,7 @@
 
 +(MapModel*) insertToDB_MapModel:(MapModel*)model{
     if (model) {
+        model.count ++;//保存时,计数器+1;
         if([MapModel insertToDB:model]) {
             return model;
         }
@@ -25,12 +26,16 @@
     NSDictionary *aWhere = [[NSDictionary alloc] initWithObjectsAndKeys:class,@"aClass",@(classId),@"aId", nil];
     MapModel *aModel = [MapModel searchSingleWithWhere:aWhere orderBy:nil];
     if (aModel) {
+        aModel.count ++;
+        [MapModel updateToDB:aModel where:[DBUtils sqlWhere_RowId:aModel.rowid]];//计数器+1并保存;
         return aModel;
     }
     //找B
     NSDictionary *bWhere = [[NSDictionary alloc] initWithObjectsAndKeys:class,@"bClass",@(classId),@"bId", nil];
     MapModel *bModel = [MapModel searchSingleWithWhere:bWhere orderBy:nil];
     if (bModel) {
+        bModel.count ++;
+        [MapModel updateToDB:bModel where:[DBUtils sqlWhere_RowId:bModel.rowid]];//计数器+1并保存;
         return bModel;
     }
     //未找到
