c93cdfcef3d483a39d87ec3c6a1b22e3486e5b40
贾晓刚 <jxg2764894@live.com>
2017-06-02 07:38:08 +0800

    DemandFactory需求工厂;(Mind产生需求)

---
+++ SMG_NothingIsAll/Class/Mind/DemandFactory/DemandFactory.h
@@ -0,0 +1,19 @@
+//
+//  DemandFactoryBase.h
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/6/2.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ *  MARK:--------------------需求工厂--------------------
+ *  1,充电需求
+ *  2,欲望需求
+ *  3,例如:看到钱时,要捡起来;并且道德打败欲望,找失主;
+ */
+@interface DemandFactory : NSObject
+
+@end
---
+++ SMG_NothingIsAll/Class/Mind/DemandFactory/DemandFactory.m
@@ -0,0 +1,13 @@
+//
+//  DemandFactoryBase.m
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/6/2.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import "DemandFactory.h"
+
+@implementation DemandFactory
+
+@end
