13c8651e187570c95731e85f5b3e4434cc52d525
贾晓刚 <jxg2764894@live.com>
2017-06-04 10:00:50 +0800

    Hobby喜好审美的设计;

---
+++ SMG_NothingIsAll/Class/Mind/Hobby/HobbyModel.h
@@ -0,0 +1,16 @@
+//
+//  HobbyModel.h
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/6/4.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ *  MARK:--------------------爱好,喜好,偏好,审美--------------------
+ */
+@interface HobbyModel : NSObject
+
+@end
---
+++ SMG_NothingIsAll/Class/Mind/Hobby/HobbyModel.m
@@ -0,0 +1,13 @@
+//
+//  HobbyModel.m
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/6/4.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import "HobbyModel.h"
+
+@implementation HobbyModel
+
+@end
