09aeccafc548be7b5e7fe6b6b31de5d5ff35f2d2
贾晓刚 <jxg2764894@live.com>
2017-07-04 06:59:18 +0800

    常识表

---
+++ SMG_NothingIsAll/Class/AIFoundation/Understand/AICommonSense.h
@@ -0,0 +1,21 @@
+//
+//  AICommonSense.h
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/7/4.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ *  MARK:--------------------常识表--------------------
+ *  1,是什么(属性)(苹果是绿色)
+ *  2,能做什么(接口)(苹果能吃)
+ *  3,
+ *  注:以AIObj表等"知识图谱表"为基础使用;
+ *  注:通过类比,来教给AI什么是颜色;然后通过"强化"来使AI确定颜色的概念;
+ */
+@interface AICommonSense : NSObject
+
+@end
---
+++ SMG_NothingIsAll/Class/AIFoundation/Understand/AICommonSense.m
@@ -0,0 +1,13 @@
+//
+//  AICommonSense.m
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/7/4.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import "AICommonSense.h"
+
+@implementation AICommonSense
+
+@end
