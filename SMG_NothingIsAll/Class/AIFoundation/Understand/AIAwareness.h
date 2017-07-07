74bd44d5171425473a16922d39d4b61f68afd8d3
贾晓刚 <jxg2764894@live.com>
2017-07-07 09:07:31 +0800

    实例...(思考思维与数据的金字塔层结构)(N3P5)

---
+++ SMG_NothingIsAll/Class/AIFoundation/Understand/AIAwareness.h
@@ -0,0 +1,17 @@
+//
+//  AIAwareness.h
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/7/7.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import "AIObject.h"
+
+/**
+ *  MARK:--------------------意识流--------------------
+ *  AIMemory的升层;(参考N3P5)
+ */
+@interface AIAwareness : AIObject
+
+@end
---
+++ SMG_NothingIsAll/Class/AIFoundation/Understand/AIAwareness.m
@@ -0,0 +1,13 @@
+//
+//  AIAwareness.m
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/7/7.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import "AIAwareness.h"
+
+@implementation AIAwareness
+
+@end
