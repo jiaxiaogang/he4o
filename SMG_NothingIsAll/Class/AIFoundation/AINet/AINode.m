a9453804633e97cb4fb04fbf1f0698e4faa598e5
jiaxiaogang <jia2764894@126.com>
2017-09-22 08:45:22 +0800

    反射节点的设计(遇到一些问题,明天解决)

--- SMG_NothingIsAll/Class/AIFoundation/AINet/AINode.m
+++ SMG_NothingIsAll/Class/AIFoundation/AINet/AINode.m
@@ -11,6 +11,9 @@
 @implementation AINode
 
 -(id) content{
+    if (POINTERISOK(_contentPointer)) {
+        return self.contentPointer.content;
+    }
     return nil;
 }
 
@@ -18,14 +21,23 @@
     if (self.type == AINodeType_Data) {
         
     }else if (self.type == AINodeType_Func) {
-        
+        //2. 反射子神经元
+        //a. 取神经元算法数据
+        AIFuncModel *funcModel = self.content;
+        if (ISOK(funcModel, AIFuncModel.class)) {
+            //b. 执行算法
+            id value = [funcModel run:content];
+            //c. 存算法值
+            //...此处需要取到另一个存储结果的神经元节点;而Func节点;必定关联了两个(1. 多功能节点; 2. 存算法结果Data节点);
+            //...两种解决方式;1. 分写几个AINode的子类; 2. 将AIPort分成四部分(传入,传出,存入,关联); 3. 保持现状,丰富type,使用判断type的方式解决;
+        }
     }else if (self.type == AINodeType_MultiFunc) {
-        //3, 功能型神经元将数据下发到子神经元
+        //3. 功能型神经元将数据下发到子神经元
         //a. 遍历当前节点网口
         for (AIPointer *pointer in ARRTOOK(self.ports)) {
             AILine *line = pointer.content;
             if (LINEISOK(line)) {
-                //b. 取此网线另一头
+                //b. 取此网线另一头节点
                 NSArray *otherPointers = [line otherPointers:pointer];
                 for (AIPointer *nodePointer in otherPointers) {
                     //c. 将数据传给另一头节点
