e32e02d0c7f4e5bbdfeced73eefd7415b0ac3a54
jiaxiaogang <jia2764894@126.com>
2017-09-27 09:11:57 +0800

    create 先天神经网络编辑器

---
+++ SMG_NothingIsAll/Class/AIFoundation/AINet/AINetEditor.h
@@ -0,0 +1,16 @@
+//
+//  AINetEditor.h
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/9/27.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ *  MARK:--------------------先天神经网络编辑器--------------------
+ */
+@interface AINetEditor : NSObject
+
+@end
---
+++ SMG_NothingIsAll/Class/AIFoundation/AINet/AINetEditor.m
@@ -0,0 +1,13 @@
+//
+//  AINetEditor.m
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/9/27.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import "AINetEditor.h"
+
+@implementation AINetEditor
+
+@end
