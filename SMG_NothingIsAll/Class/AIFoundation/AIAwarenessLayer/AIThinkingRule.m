0f370d24dc66d853a481debe5e81f3e2ddabb6c4
jiaxiaogang <jia2764894@126.com>
2017-11-14 08:36:42 +0800

    none

--- SMG_NothingIsAll/Class/AIFoundation/AIAwarenessLayer/AIThinkingRule.m
+++ SMG_NothingIsAll/Class/AIFoundation/AIAwarenessLayer/AIThinkingRule.m
@@ -39,9 +39,12 @@
     if (ISOK(data, NSString.class)) {
         [theNet commitString:data];//潜思维,识别;
     }else if(ISOK(data, AIHungerLevelChangedModel.class)){
-        NSLog(@"");
+        //1. data的信息总会经过神经元生成为神经网络;只是无关联会很快GC掉;
+        [theNet commitModel:data];
+        //2. mindValue提纯后,用于构建神经网络关联;
+        //3. mindVaue能否被存储?(应该会存储自我的mindValue快乐状态,mindValue具有影响构建和可数据表示二象性,mindValue原本不是一条数据,但因被自我状态感知了,才变成数据)
     }else if(ISOK(data, AIHungerStateChangedModel.class)){
-        NSLog(@"");
+        [theNet commitModel:data];
     }
 }
 -(void) activityByDeep:(id)data{
