b5e991e27393f6e064771f0d5037cb314573a248
jiaxiaogang <jia2764894@126.com>
2017-11-21 09:04:03 +0800

    事务控制器

---
+++ SMG_NothingIsAll/Class/AIFoundation/AIAwarenessLayer/AIThalamusRule.m
@@ -0,0 +1,13 @@
+//
+//  AIThalamusRule.m
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/11/21.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import "AIThalamusRule.h"
+
+@implementation AIThalamusRule
+
+@end
