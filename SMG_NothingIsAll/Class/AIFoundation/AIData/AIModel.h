7e8413b321c7c81f8bbd8c2e391359323dc83328
jiaxiaogang <jia2764894@126.com>
2017-12-05 09:11:00 +0800

    AIModel数据模型;

---
+++ SMG_NothingIsAll/Class/AIFoundation/AIData/AIModel.h
@@ -0,0 +1,17 @@
+//
+//  AIModel.h
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/12/5.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import "AIObject.h"
+
+/**
+ *  MARK:--------------------存AIDataNode的数据模型--------------------
+ *  参考:n9p9 AINet(数据模型)
+ */
+@interface AIModel : AIObject
+
+@end
---
+++ SMG_NothingIsAll/Class/AIFoundation/AIData/AIModel.m
@@ -0,0 +1,13 @@
+//
+//  AIModel.m
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/12/5.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import "AIModel.h"
+
+@implementation AIModel
+
+@end
