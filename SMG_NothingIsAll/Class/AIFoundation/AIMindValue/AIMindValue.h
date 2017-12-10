6cebf74ef12a74cd9c0364e099a50bdcf55aaed0
jiaxiaogang <jia2764894@126.com>
2017-12-10 11:24:21 +0800

    mindValue规则与思维融合;

---
+++ SMG_NothingIsAll/Class/AIFoundation/AIMindValue/AIMindValue.h
@@ -0,0 +1,16 @@
+//
+//  AIMindValue.h
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/12/10.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ *  MARK:--------------------mindValue--------------------
+ */
+@interface AIMindValue : NSObject
+
+@end
---
+++ SMG_NothingIsAll/Class/AIFoundation/AIMindValue/AIMindValue.m
@@ -0,0 +1,13 @@
+//
+//  AIMindValue.m
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/12/10.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import "AIMindValue.h"
+
+@implementation AIMindValue
+
+@end
