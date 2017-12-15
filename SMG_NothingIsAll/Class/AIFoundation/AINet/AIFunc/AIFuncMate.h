e1d10a8faf448681efa97fc06874ed53e48def4e
jiaxiaogang <jia2764894@126.com>
2017-12-15 09:31:26 +0800

    定义AIFuncMate算法伴侣;

---
+++ SMG_NothingIsAll/Class/AIFoundation/AINet/AIFunc/AIFuncMate.h
@@ -0,0 +1,22 @@
+//
+//  AINetMate.h
+//  SMG_NothingIsAll
+//
+//  Created by 贾  on 2017/12/15.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ *  MARK:--------------------算法伴侣--------------------
+ *  参考:n9p17
+ */
+@interface AIFuncMate : NSObject
+
+@end
+
+
+@interface AIFuncMateDemo : NSObject
+
+@end
