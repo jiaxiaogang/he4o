bda01c0ec642e3185a140e513ad92d7a6f429b5a
jiaxiaogang <jia2764894@126.com>
2017-12-23 15:02:04 +0800

    Thinking.Cache;

--- SMG_NothingIsAll/Class/AIFoundation/AIThinkingControl/AIThinkingControl.m
+++ SMG_NothingIsAll/Class/AIFoundation/AIThinkingControl/AIThinkingControl.m
@@ -11,8 +11,11 @@
 #import "AIHungerLevelChangedModel.h"
 #import "AIHungerStateChangedModel.h"
 #import "AIInputMindValue.h"
+#import "AIMindValue.h"
 
 @interface AIThinkingControl()
+
+@property (strong,nonatomic) NSMutableArray *caches;
 
 @end
 
@@ -36,6 +39,7 @@ static AIThinkingControl *_instance;
 }
 
 -(void) initData{
+    self.caches = [[NSMutableArray alloc] init];
 }
 
 -(void) initRun{
@@ -46,17 +50,28 @@ static AIThinkingControl *_instance;
 //MARK:===============================================================
 -(void) activityByShallow:(id)data{
     if (ISOK(data, NSString.class)) {
-        [theNet commitString:data];//潜思维,识别;
-    }else if(ISOK(data, AIHungerLevelChangedModel.class)){
+        NSLog(@"_________shadowForNoMV");//无mv
+        //1. 尝试从caches取mv邻居;
+        
+        
+        //2. 潜思维,识别;
+        [theNet commitString:data];
+        
+        
+    }else if(ISOK(data, AIInputMindValue.class)) {
+        NSLog(@"_________shadowForIMV");//有mv...
+        //1. 思维对imv发生时,前后4个左右的思维缓存区作分析与关联操作;
+        [self addObjectToCaches:data];
+        
+        //2. 与caches作关联分析;
+        
+    }else if(ISOK(data, AIHungerLevelChangedModel.class)) {
         //1. data的信息总会经过神经元生成为神经网络;只是无关联会很快GC掉;
         [theNet commitModel:data];
         //2. mindValue提纯后,用于构建神经网络关联;
         //3. mindVaue能否被存储?(应该会存储自我的mindValue快乐状态,mindValue具有影响构建和可数据表示二象性,mindValue原本不是一条数据,但因被自我状态感知了,才变成数据)
     }else if(ISOK(data, AIHungerStateChangedModel.class)){
         [theNet commitModel:data];
-    }else if(ISOK(data, AIInputMindValue.class)){
-        NSLog(@"_________shadowForIMV");
-        //思维对imv发生时,前后4个左右的思维缓存区作分析与关联操作;
     }
 }
 -(void) activityByDeep:(id)data{
@@ -64,6 +79,19 @@ static AIThinkingControl *_instance;
 }
 -(void) activityByNone:(id)data{
     NSLog(@"创建后台任务");
+}
+
+-(void) addObjectToCaches:(id)data{
+    if (data) {
+        [self.caches addObject:data];
+    }
+    if (self.caches.count > 4) {
+        [self.caches removeObjectAtIndex:0];
+    }
+}
+
+-(BOOL) objectHavMV:(id)data{
+    return data && (ISOK(data, AIInputMindValue.class) || ISOK(data, AIMindValue.class));
 }
 
 @end
