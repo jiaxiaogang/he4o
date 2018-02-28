51fc8d55937a29b1699e1f6a172f79661b5fac56
jiaxiaogang <jxg2764894@live.com>
2018-02-28 09:05:20 +0800

    中断针对algsDic的开发进程,准备转向RIN;

--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/AIThinkingControl.m
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/AIThinkingControl.m
@@ -85,6 +85,11 @@ static AIThinkingControl *_instance;
         
     }
     
+    
+    //因algsDic定义只是存储结构,并非归纳结构,所以应类比Law,并thinkingRIN后,再产生归纳结构网络;//xxx
+    //shortCaches和longCaches中存储的也是RIN后的数据,而非algsDic;//xxx
+    
+    
     //3. 存cacheShort
     [self setObject_Caches:nodeDic];
     
