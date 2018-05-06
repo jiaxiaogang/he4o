7ff89700dfd57c2049696196adb056daf1f77506
赤臂生 <jxg2764894@live.com>
2018-05-06 14:48:58 +0800

    none

--- SMG_NothingIsAll/AIFoundation/AINet/AINetCMV/AINetCMV.m
+++ SMG_NothingIsAll/AIFoundation/AINet/AINetCMV/AINetCMV.m
@@ -17,6 +17,11 @@
     
     //2.
     
+    //3. order与mv都打包成node;
+    
+    //4. order与mv都可以独立抽象;
+    
+    //5. 可以考虑将order作为mvNode的一个 "orderArr"存在;(或者说以timeWeight和strongWeight)存两个不同的排序方案
     
     
     
