307db7c7955eee6bc4acdbbc65738c1d5db6b135
赤臂生 <jxg2764894@live.com>
2018-05-07 09:12:23 +0800

    none

---
+++ SMG_NothingIsAll/AIFoundation/AINet/AINetCMV/AIOrderNode.h
@@ -0,0 +1,16 @@
+//
+//  AIOrderNode.h
+//  SMG_NothingIsAll
+//
+//  Created by iMac on 2018/5/7.
+//  Copyright © 2018年 XiaoGang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//MARK:===============================================================
+//MARK:                     < node有序集合 >
+//MARK:===============================================================
+@interface AIOrderNode : NSObject
+
+@end
---
+++ SMG_NothingIsAll/AIFoundation/AINet/AINetCMV/AIOrderNode.m
@@ -0,0 +1,13 @@
+//
+//  AIOrderNode.m
+//  SMG_NothingIsAll
+//
+//  Created by iMac on 2018/5/7.
+//  Copyright © 2018年 XiaoGang. All rights reserved.
+//
+
+#import "AIOrderNode.h"
+
+@implementation AIOrderNode
+
+@end
