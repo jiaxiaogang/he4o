c4974e8895b9fc74a54e839b9d55e1d5282b7d23
赤臂生 <jxg2764894@live.com>
2018-05-15 09:31:41 +0800

    明日计划:写第四序列的抽象;

--- SMG_NothingIsAll/AIFoundation/AINet/AINetCMV/AINetCMV.h
+++ SMG_NothingIsAll/AIFoundation/AINet/AINetCMV/AINetCMV.h
@@ -39,6 +39,8 @@
 @property (strong, nonatomic) NSMutableArray *orders_kvp; //在imv前发生的noMV的algs数据序列;(前因序列)(使用kvp而不是port的原因是cmvModel的强度不变:参考n12p16)
 @property (strong, nonatomic) AIKVPointer *cmvPointer;      //
 
+-(void) create;
+
 @end
 
 
