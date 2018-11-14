bd8da51df7edfeda01ad656c17c4e87c50af59f1
赤臂生 <jxg2764894@live.com>
2018-11-15 07:42:13 +0800

    将远投改为自定义坐标的方式;(完成)

--- SMG_NothingIsAll/Demo/BirdGrowPage/BirdGrowPage.m
+++ SMG_NothingIsAll/Demo/BirdGrowPage/BirdGrowPage.m
@@ -14,7 +14,8 @@
 
 @property (strong,nonatomic) BirdView *birdView;
 @property (strong,nonatomic) UITapGestureRecognizer *farTapRecognizer;
-@property (weak, nonatomic) IBOutlet UILabel *farLab;
+@property (weak, nonatomic) IBOutlet UIView *farView;
+@property (weak, nonatomic) IBOutlet UIView *borderView;
 
 @end
 
@@ -31,25 +32,18 @@
     [self.view addSubview:self.birdView];
     
     //farTapRecognizer
-    self.farTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(farTapAction:)];
-    [self.farLab addGestureRecognizer:self.farTapRecognizer];
+    self.farTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(customTapAction:)];
+    [self.farView addGestureRecognizer:self.farTapRecognizer];
     self.farTapRecognizer.delegate = self;
+    
+    //borderView
+    [self.borderView.layer setBorderColor:[UIColor grayColor].CGColor];
+    [self.borderView.layer setBorderWidth:30];
 }
 
 //MARK:===============================================================
 //MARK:                     < onclick >
 //MARK:===============================================================
-- (IBAction)farFeedingBtnOnClick:(id)sender {
-    FoodView *foodView = [[FoodView alloc] init];
-    [foodView hit];
-    [foodView setOrigin:CGPointMake(ScreenWidth * 0.25f, ScreenHeight - 66)];
-    [self.view addSubview:foodView];
-    CGPoint targetPoint = CGPointMake(ScreenWidth * 0.5f, ScreenHeight * 0.5f);
-    [UIView animateWithDuration:1.0f animations:^{
-        [foodView setOrigin:targetPoint];
-    }];
-}
-
 - (IBAction)nearFeedingBtnOnClick:(id)sender {
     FoodView *foodView = [[FoodView alloc] init];
     [foodView hit];
@@ -71,9 +65,24 @@
 //MARK:===============================================================
 //MARK:                     < method >
 //MARK:===============================================================
-- (void)farTapAction:(UITapGestureRecognizer *)tapRecognizer{
-    NSLog(@"远投");
-    //CGPoint point = [tapRecognizer locationInView:tapRecognizer.view];
+- (void)customTapAction:(UITapGestureRecognizer *)tapRecognizer{
+    UIView *tapView = tapRecognizer.view;
+    CGPoint point = [tapRecognizer locationInView:tapView];
+    CGFloat xRate = point.x / tapView.width;
+    CGFloat yRate = point.y / tapView.height;
+    CGFloat targetX = 30 + (ScreenWidth - 60) * xRate;
+    CGFloat targetY = 94 + (ScreenHeight - 60 - 128) * yRate;
+    NSLog(@"远投:%f,%f",xRate,yRate);
+    
+    FoodView *foodView = [[FoodView alloc] init];
+    [foodView hit];
+    [foodView setOrigin:CGPointMake(ScreenWidth * 0.25f, ScreenHeight - 66)];
+    [self.view addSubview:foodView];
+    CGPoint targetPoint = CGPointMake(targetX, targetY);
+    
+    [UIView animateWithDuration:1.0f animations:^{
+        [foodView setOrigin:targetPoint];
+    }];
 }
 
 @end
