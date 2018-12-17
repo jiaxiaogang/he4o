3c722b8bf591d5ec018c6b089a7f23699a97085a
赤臂生 <jxg2764894@live.com>
2018-12-17 08:21:09 +0800

    继续写AIAlgNodeManager (未完成);

--- SMG_NothingIsAll/AIFoundation/AINet/AIAlgNodeManager/AIAlgNodeManager.m
+++ SMG_NothingIsAll/AIFoundation/AINet/AIAlgNodeManager/AIAlgNodeManager.m
@@ -11,6 +11,7 @@
 #import "AIAbsAlgNode.h"
 #import "AIPort.h"
 #import "AIKVPointer.h"
+#import "AINetUtils.h"
 
 @implementation AIAlgNodeManager
 
@@ -21,6 +22,12 @@
         AIAbsAlgNode *absNode = [[AIAbsAlgNode alloc] init];
         absNode.pointer = [SMGUtils createPointerForNode:PATH_NET_ALG_ABS_NODE];
         absNode.value_p = alg_p;/////TODO引用序列去重给用上;
+        
+        
+        
+        
+        
+        
         [absAlgNodes addObject:absNode];
     }
     
@@ -29,21 +36,18 @@
     if (!conNode) {
         conNode = [[AIAlgNode alloc] init];
         conNode.pointer = [SMGUtils createPointerForNode:PATH_NET_ALG_NODE];
-        
     }
     
+    //3. 关联
+    for (AIAbsAlgNode *absNode in absAlgNodes) {
+        [AINetUtils insertPointer:absNode.pointer toPorts:conNode.absPorts];
+        [AINetUtils insertPointer:conNode.pointer toPorts:absNode.conPorts];
+    }
     
-    
-    //
-    //    //3. 关联
-    //    [self insertPointer:absNode.pointer toPorts:conNode.absPorts];
-    //    [self insertPointer:conNode.pointer toPorts:absNode.conPorts];
-    //
-    //    //4. 存储抽象节点
-    //    [SMGUtils insertObject:absNode rootPath:absNode.pointer.filePath fileName:FILENAME_Node time:cRedisNodeTime];
-    
-    
-    
+    //4. 存储抽象节点
+    for (AIAbsAlgNode *absNode in absAlgNodes) {
+        [SMGUtils insertObject:absNode rootPath:absNode.pointer.filePath fileName:FILENAME_Node time:cRedisNodeTime];
+    }
     
     //5. 存储具象节点
     [SMGUtils insertObject:conNode rootPath:conNode.pointer.filePath fileName:FILENAME_Node time:cRedisNodeTime];
@@ -56,6 +60,10 @@
 //MARK:                     < private_Method >
 //MARK:===============================================================
 
+/**
+ *  MARK:--------------------查找网络中即有具象节点--------------------
+ *  注: 性能优化:如果在查找过程中,共同具象节点有上百个,而又因数量等不符合,则会有卡在IO上的可能;所以此处届时可考虑使用二分法索引等来优化;
+ */
 +(AIAlgNode*) findLocalConNode:(NSArray*)absNodes{
     //1.  数据准备
     absNodes = ARRTOOK(absNodes);
---
+++ SMG_NothingIsAll/AIFoundation/AINet/AINetHeader.h
@@ -0,0 +1,15 @@
+//
+//  AINetHeader.h
+//  SMG_NothingIsAll
+//
+//  Created by jia on 2018/12/17.
+//  Copyright © 2018年 XiaoGang. All rights reserved.
+//
+
+#import "AINetUtils.h"
+
+#import "AIPointer.h"
+#import "AIKVPointer.h"
+#import "AISqlPointer.h"
+
+#import "AIPort.h"
