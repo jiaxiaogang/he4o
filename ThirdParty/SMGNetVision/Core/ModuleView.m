ae1e2b72c6d32e3540f101bc59f6aebcb97f2cb8
赤臂生 <jxg2764894@live.com>
2019-06-16 08:21:40 +0800

    神经网络可视化之节点排版算法完成 (未测试);

--- ThirdParty/SMGNetVision/Core/ModuleView.m
+++ ThirdParty/SMGNetVision/Core/ModuleView.m
@@ -93,22 +93,47 @@
  *  1. 有可能,a组与b组间没抽具象关系;此时只能默认往底部排;
  */
 -(void) refreshDisplay_Node{
-    //1. 获取分组数据;
-    NSArray *sortGroups = [self getSortGroups];
+    //1. 找出所有有关系的NodeCompareModel
+    NSArray *compareModels = [self getNodeCompareModels];
     
-    //2. 转成编层号字典;
+    //2. 获取分组数据;
+    NSArray *sortGroups = [self getSortGroups:compareModels];
+    
+    //3. 转成编层号字典; (每组的y单独从0开始,各组的x都要累加)
     NSMutableDictionary *xDic = [[NSMutableDictionary alloc] init];//从左往右编号
     NSMutableDictionary *yDic = [[NSMutableDictionary alloc] init];//从下往上编号
-    int curX = 0;
-    int curY = 0;
-    ///1. 有抽象加一层的,
-    ///2. 也有平层在同一层的;
+    int curX = -1;
+    for (NSArray *sortGroup in sortGroups) {
+        int curY = 0;
+        //判断当前sortItem是否是上一个last的抽象; (是则curY+1,不是则curY+0)
+        id lastSortItem = nil;
+        for (id sortItem in sortGroup) {
+            NSComparisonResult compare = [self compareNodeData1:sortItem nodeData2:lastSortItem compareModels:compareModels];
+            ///1. 排y_抽象加一层;
+            if (compare == NSOrderedAscending) {
+                [yDic setObject:@(++curY) forKey:sortItem];
+            }else if(compare == NSOrderedSame){
+                ///2. 排y_平层在同一层;
+                [yDic setObject:@(curY) forKey:sortItem];
+            }else{
+                NSLog(@"错误!!! 排更后面的节点,不允许比具象更具象! (请检查排序算法,是否排反了)");
+            }
+            ///3. 排x;
+            [xDic setObject:@(++curX) forKey:sortItem];
+            
+            ///4. 记录last,下一个;
+            lastSortItem = sortItem;
+        }
+    }
     
     //3. 根据编号计算坐标;
-    
-    
-    
-    
+    NSArray *nodeViews = ARRTOOK([self subViews_AllDeepWithClass:NodeView.class]);
+    for (NodeView *nodeView in nodeViews) {
+        NSInteger x = [NUMTOOK([xDic objectForKey:nodeView.data]) integerValue];
+        NSInteger y = [NUMTOOK([yDic objectForKey:nodeView.data]) integerValue];
+        [nodeView setX:x * 12];
+        [nodeView setX:(self.height - y * 12)];
+    }
 }
 
 /**
@@ -116,9 +141,9 @@
  *  注: 其中最具象为0,抽象往上,越抽象值越大,越具象值越小;
  *  @result : 二维数组,元素为组,组中具象在前,抽象在后;
  */
--(NSMutableArray*) getSortGroups{
-    //1. 找出所有有关系的NodeCompareModel
-    NSArray *compareModels = [self getNodeCompareModels];
+-(NSMutableArray*) getSortGroups:(NSArray*)compareModels{
+    //1. 数据检查
+    compareModels = ARRTOOK(compareModels);
     
     //2. 用相容算法,分组;
     NSMutableArray *groups = [[NSMutableArray alloc] init];
@@ -141,7 +166,7 @@
         }
     }
     
-    //3. 对groups中,每一组进行独立排序,并取编号结果;
+    //3. 对groups中,每一组进行独立排序,并取编号结果; (排序:从具象到抽象)
     NSMutableArray *sortGroups = [[NSMutableArray alloc] init];
     for (NSArray *group in groups) {
         NSArray *sortGroup = [group sortedArrayUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
@@ -155,6 +180,7 @@
 /**
  *  MARK:--------------------比较nodeData1和2的抽具象关系--------------------
  *  @result : 抽象为大,具象为小,无关系为相等
+ *  @desc : 排序规则: (从具象到抽象 / 从小到大)
  */
 -(NSComparisonResult)compareNodeData1:(id)n1 nodeData2:(id)n2 compareModels:(NSArray*)compareModels{
     //1. 数据检查
