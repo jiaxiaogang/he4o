35eded6a138209db760d975beaf03cace8c65247
赤臂生 <jxg2764894@live.com>
2019-12-26 08:02:27 +0800

    重构出AIThinkOutAnalogy类比器;

--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/TOAlgScheme.m
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/TOAlgScheme.m
@@ -15,6 +15,7 @@
 #import "AINetIndex.h"
 #import "AIShortMatchModel.h"
 #import "AINetIndexUtils.h"
+#import "AIThinkOutAnalogy.h"
 
 @interface TOAlgScheme()
 
@@ -322,7 +323,7 @@
         BOOL cIsAbs = ISOK(curAlg, AIAbsAlgNode.class);
         NSArray *cConPorts = cIsAbs ? ((AIAbsAlgNode*)curAlg).conPorts : nil;
         BOOL mIsC = [SMGUtils containsSub_p:matchAlg.pointer parentPorts:cConPorts];
-        [self mcAnalogy:matchAlg cAlg:curAlg complete:^(NSArray *mcs, NSArray *ms, NSArray *cs) {
+        [AIThinkOutAnalogy mcAnalogy:matchAlg cAlg:curAlg complete:^(NSArray *mcs, NSArray *ms, NSArray *cs) {
             //2. 数据准备: (mcs无效且m不抽象自C时 = 则不匹配)
             if (!ARRISOK(mcs) && !mIsC) {
                 return;
@@ -386,45 +387,6 @@
             }
         }];
     }
-}
-
-
-/**
- *  MARK:--------------------MC类比--------------------
- *  @param complete : return mcs&ms&cs notnull
- *  @desc 缩写说明: m = matchAlg, c = curAlg, mcs = MCSame, ms = MSpecial, cs = CSpecial
- */
--(void) mcAnalogy:(AIAlgNodeBase*)mAlg cAlg:(AIAlgNodeBase*)cAlg complete:(void(^)(NSArray *mcs,NSArray *ms,NSArray *cs))complete{
-    //1. 数据准备
-    if (!mAlg || !cAlg) {
-        return;
-    }
-    NSMutableArray *mcs = [[NSMutableArray alloc] init];
-    NSMutableArray *ms = [[NSMutableArray alloc] init];
-    NSMutableArray *cs = [[NSMutableArray alloc] init];
-    
-    //2. 收集m_ps & c_ps & mc_ps;
-    NSArray *mAbs_ps = [SMGUtils convertPointersFromPorts:mAlg.absPorts];
-    NSArray *cAbs_ps = [SMGUtils convertPointersFromPorts:cAlg.absPorts];
-    NSMutableArray *mcAbs_ps = [[NSMutableArray alloc] init];
-    [mcAbs_ps addObjectsFromArray:mAbs_ps];
-    [mcAbs_ps addObjectsFromArray:cAbs_ps];
-    
-    //3. 收集mcs & ms & cs;
-    for (AIPointer *item_p in mcAbs_ps) {
-        BOOL mContains = [SMGUtils containsSub_p:item_p parent_ps:mAbs_ps];
-        BOOL cContains = [SMGUtils containsSub_p:item_p parent_ps:cAbs_ps];
-        if (mContains && cContains) {
-            if (![SMGUtils containsSub_p:item_p parent_ps:mcs]) [mcs addObject:item_p];
-        }else if(mContains){
-            if (![SMGUtils containsSub_p:item_p parent_ps:ms]) [ms addObject:item_p];
-        }else if(cContains){
-            if (![SMGUtils containsSub_p:item_p parent_ps:cs]) [cs addObject:item_p];
-        }
-    }
-    
-    //4. 返回_MC进行评价;
-    if (complete) complete(mcs,ms,cs);
 }
 
 /**
