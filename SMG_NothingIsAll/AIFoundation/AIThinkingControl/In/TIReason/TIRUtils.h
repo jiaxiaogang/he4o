3963eda1e9c74bb36ddf8c72d108385652a6d908
赤臂生 <jxg2764894@live.com>
2020-01-10 09:29:57 +0800

    修复时序识别老失败问题: 写TIR中两个时序匹配度判断 (未完成);

---
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/In/TIReason/TIRUtils.h
@@ -0,0 +1,13 @@
+//
+//  TIRUtils.h
+//  SMG_NothingIsAll
+//
+//  Created by jia on 2020/1/10.
+//  Copyright © 2020年 XiaoGang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TIRUtils : NSObject
+
+@end
---
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/In/TIReason/TIRUtils.m
@@ -0,0 +1,20 @@
+//
+//  TIRUtils.m
+//  SMG_NothingIsAll
+//
+//  Created by jia on 2020/1/10.
+//  Copyright © 2020年 XiaoGang. All rights reserved.
+//
+
+#import "TIRUtils.h"
+
+@implementation TIRUtils
+
+/**
+ *  MARK:--------------------对protoFo和assFo进行match判断--------------------
+ */
++(void) TIR_Fo_CheckMatch:(AIFoNodeBase*)protoFo assFo:(AIFoNodeBase*)assFo success:(void(^)(NSInteger matchCount,NSInteger lastMatchIndex))success{
+    //1. 数据准备;
+}
+
+@end
