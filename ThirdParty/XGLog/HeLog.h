d867efabc5b1bbd2970dcb568123ecac0402b804
赤臂生 <jxg2764894@live.com>
2020-03-12 06:21:43 +0800

    写HeLog用来做持久化日志服务;

---
+++ ThirdParty/XGLog/HeLog.h
@@ -0,0 +1,41 @@
+//
+//  HeLog.h
+//  SMG_NothingIsAll
+//
+//  Created by jia on 2020/3/12.
+//  Copyright © 2020年 XiaoGang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define kTime @"t"
+#define kLog @"l"
+#define kFolderName @"helog"
+#define kFileName @"datas"
+
+/**
+ *  MARK:--------------------XGWedis--------------------
+ *  说明:
+ *  1. XGWedis用来做异步持久化;
+ *  2. 目前每10s持久化一次;
+ *  3. XGWedis支持delegate/observer/block三种持久化方式;
+ *
+ */
+@interface HeLog : NSObject
+
++(HeLog*) sharedInstance;
+
+/**
+ *  MARK:--------------------addLog--------------------
+ */
+-(void) addLog:(NSString*)log;
+-(NSArray*) getDatas;
+
+/**
+ *  MARK:--------------------filter--------------------
+ *  @param startT : 格式为yyyyMMddHHmmssSSS 如: 20200312055959000
+ */
+-(NSArray*) filterByTime:(NSString*)startT endT:(NSString*)endT;
+-(NSArray*) filterByKeyword:(NSString*)keyword;
+
+@end
---
+++ ThirdParty/XGLog/HeLog.m
@@ -0,0 +1,123 @@
+//
+//  HeLog.m
+//  SMG_NothingIsAll
+//
+//  Created by jia on 2020/3/12.
+//  Copyright © 2020年 XiaoGang. All rights reserved.
+//
+
+#import "HeLog.h"
+#import "PINCache.h"
+
+@interface HeLog ()
+
+@property (strong, nonatomic) NSMutableArray *datas;
+@property (strong,nonatomic) NSTimer *timer;            //计时器
+
+@end
+
+@implementation HeLog
+
+static HeLog *_instance;
++(HeLog*) sharedInstance{
+    if (_instance == nil) {
+        _instance = [[HeLog alloc] init];
+    }
+    return _instance;
+}
+
+-(id) init{
+    self = [super init];
+    if (self) {
+        [self initData];
+    }
+    return self;
+}
+
+-(void) initData{
+    PINDiskCache *cache = [[PINDiskCache alloc] initWithName:kFolderName];
+    id file = [cache objectForKey:kFileName];
+    self.datas = [[NSMutableArray alloc] initWithArray:file];
+    NSLog(@"===========HeLog Init Data %ld============",self.datas.count);
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:5 target:self selector:@selector(notificationTimer) userInfo:nil repeats:YES];
+}
+
+
+//MARK:===============================================================
+//MARK:                     < publicMethod >
+//MARK:===============================================================
+/**
+ *  MARK:--------------------addLog--------------------
+ */
+-(void) addLog:(NSString*)log{
+    log = STRTOOK(log);
+    long long nowTime = [[NSDate date] timeIntervalSince1970];
+    [self.datas addObject:@{kTime:@(nowTime),kLog:log}];
+}
+-(NSArray*) getDatas{
+    return self.datas;
+}
+
+/**
+ *  MARK:--------------------filter--------------------
+ */
+-(NSArray*) filterByTime:(NSString*)startT endT:(NSString*)endT{
+    //1. 转换startT和endT的时间戳;
+    NSDateFormatter *format = [[NSDateFormatter alloc] init];
+    format.dateFormat = @"yyyyMMddHHmmssSSS";
+    NSDate *startDate = [format dateFromString:startT];
+    NSDate *endDate = [format dateFromString:endT];
+    if (!startDate || !endDate) {
+        ELog(@"输入时间格式错误!!! (%@,%@)",startT,endT);
+        return nil;
+    }
+    long long startTime = [startDate timeIntervalSince1970];
+    long long endTime = [endDate timeIntervalSince1970];
+    
+    //2. 找起始index
+    NSInteger startIndex = 0;
+    NSInteger endIndex = self.datas.count - 1;
+    for (NSDictionary *item in self.datas) {
+        long long itemTime = [NUMTOOK([item objectForKey:kTime]) longLongValue];
+        if (itemTime >= startTime) {
+            startIndex = [self.datas indexOfObject:item];
+        }
+        if (itemTime == endTime) {
+            endIndex = [self.datas indexOfObject:item];
+        }else if(itemTime < endTime){
+            endIndex = [self.datas indexOfObject:item] - 1;
+        }
+    }
+    
+    //3. 截取
+    NSInteger length = endIndex - startIndex + 1;
+    return ARR_SUB(self.datas, startIndex,length);
+}
+
+-(NSArray*) filterByKeyword:(NSString*)keyword{
+    //1. 数据准备
+    keyword = STRTOOK(keyword);
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    
+    //2. 筛选
+    for (NSDictionary *item in self.datas) {
+        NSString *log = [item objectForKey:kLog];
+        if ([log containsString:keyword]) {
+            [result addObject:item];
+        }
+    }
+    return result;
+}
+
+//MARK:===============================================================
+//MARK:                     < privateMethod >
+//MARK:===============================================================
+- (void)notificationTimer{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        PINDiskCache *cache = [[PINDiskCache alloc] initWithName:kFolderName];
+        [cache setObject:self.datas forKey:kFileName];
+        //dispatch_async(dispatch_get_main_queue(), ^{});
+    });
+}
+
+@end
