5d32849a120f6c4bb6be0eb24f1eb0b975d39a15
赤臂生 <jxg2764894@live.com>
2020-05-20 07:23:04 +0800

    规划重构新的AIThinkOutAction行为化类;

---
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/AIThinkOutAction.h
@@ -0,0 +1,25 @@
+//
+//  AIThinkOutAction.h
+//  SMG_NothingIsAll
+//
+//  Created by jia on 2020/5/20.
+//  Copyright © 2020年 XiaoGang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ *  MARK:--------------------行为化类--------------------
+ *  @version 20200520由TOAlgScheme大改版而来;
+ *  @desc
+ *      1. 支持两个主要入口方法:
+ *          a. SP_Value
+ *          b. P_Alg
+ *      2. 支持子时序递归 (当cHav时,转移至另一时序);
+ *      3. 支持outModel:
+ *          a. 行为化成功时,cutIndex右移至下帧;
+ *          b. 转移时,将subOutModel存至outModel;
+ */
+@interface AIThinkOutAction : NSObject
+
+@end
---
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/AIThinkOutAction.m
@@ -0,0 +1,13 @@
+//
+//  AIThinkOutAction.m
+//  SMG_NothingIsAll
+//
+//  Created by jia on 2020/5/20.
+//  Copyright © 2020年 XiaoGang. All rights reserved.
+//
+
+#import "AIThinkOutAction.h"
+
+@implementation AIThinkOutAction
+
+@end
