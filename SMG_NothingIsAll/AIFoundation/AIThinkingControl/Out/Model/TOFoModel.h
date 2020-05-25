ab0fd70afa9b65ca2753d700397b2d3b3aa0f3c9
jiaxiaogang <jxg2764894@live.com>
2020-05-26 05:19:01 +0800

    整理TOModel;

--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/Model/TOFoModel.h
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/Model/TOFoModel.h
@@ -13,9 +13,26 @@
  *  1. content_p : 存AIFoNodeBase_p
  *  2. 再通过algScheme联想把具象可执行的具体任务存到memOrder;
  *  3. 其间,如果有执行失败,无效等概念节点,存到except_ps不应期;
+ *  4. 不应期会上报给上一级except_ps (或许由TOModelStatus来替代此功能);
  */
 @interface TOFoModel : TOModelBase
 
 @property (strong, nonatomic) NSMutableArray *actions;//行为化数据;
+
+@property (assign, nonatomic) NSInteger actionIndex;                //当前正在行为化的下标;
+@property (strong, nonatomic) NSMutableDictionary *itemSubModels;   //每个下标,对应的subModels字典;
+
+
+/**
+ *  MARK:--------------------当前正在激活中的subModel--------------------
+ *  可由status来替代此功能 (status可支持多个激活状态的fo);
+ */
+@property (strong, nonatomic) TOModelBase *activateSubModel;
+
+
+/**
+ *  MARK:--------------------当前model的状态--------------------
+ */
+@property (assign, nonatomic) TOModelStatus status;
 
 @end
