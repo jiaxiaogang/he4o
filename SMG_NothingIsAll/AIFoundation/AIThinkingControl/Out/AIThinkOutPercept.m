9542664f34f229f63c35e04dfc480e7d6a9e44a6
jiaxiaogang <jxg2764894@live.com>
2020-06-02 07:09:39 +0800

    写新输入推进决策进度 (未完成);

--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/AIThinkOutPercept.m
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/AIThinkOutPercept.m
@@ -53,52 +53,13 @@
     NSArray *mModels = [self.delegate aiTOP_GetShortMatchModel];
     if (!demand || !ARRISOK(mModels)) return;
     
-    //2. 优先最新一帧,与上轮循环做匹配 (对单帧Finish的,要在下轮input传回判断是否符合要求,并跳转至下帧);
-    
-    //a. 输出行为的actYes要进行下轮匹配,比如吃,确定自己是否真吃了;
-    //b. 未输出行为,等待中的,也要进行下轮匹配,比如等开饭,等来开饭了; (等待的status是ActNo还是Runing?)
+    //2. 外循环入->推进->中循环出;
     AIShortMatchModel *latestMModel = ARR_INDEX_REVERSE(mModels, 0);
     if (latestMModel) {
-        //b. 取出所有等待下轮的outModel;
-        NSArray *waitModels = [TOUtils getSubOutModels_AllDeep:demand validStatus:@[@(TOModelStatus_ActYes),@(TOModelStatus_Runing)]];
+        [self outerPushMiddleLoop:demand latestMatchAlg:latestMModel.matchAlg];
+        //TODOTOMORROW:
+        //此处推进成功后,下面的四模式不必运行,
         
-        //c. 判断最近一次input是否与等待中outModel相匹配;
-        for (TOModelBase *waitModel in waitModels) {
-            if ([TOUtils mIsC_1:latestMModel.matchAlg.pointer c:waitModel.content_p]) {
-                
-                //d. 匹配,则完成;
-                waitModel.status = TOModelStatus_Finish;
-                
-                //e. 并反馈给上一级,跳到下帧,
-                TOModelBase *groupModel = waitModel.baseOrGroup;
-                if (ISOK(groupModel, TOFoModel.class)) {
-                    TOFoModel *toFoModel = (TOFoModel*)groupModel;
-                    toFoModel.actionIndex ++;
-                    [self.delegate aiTOP_2TOR_ReasonPlus:toFoModel];
-                    
-                    //f. foModel全完成了,则再递归至上一层;
-                    TOModelBase *baseModel = toFoModel.baseOrGroup;
-                    baseModel.status = TOModelStatus_Finish;
-                    
-                    //baseModel有可能是value也有可能是alg,再取group的话,有可能取到alg或fo;
-                    //如果取到alg,则应将当前已完成的value标记到algOutModel.alreadyFinishs,并提给TOAction._P/_SP继续完成去;
-                    //如果取到fo,则下帧继续;
-                    
-                    //TODOTOMORROW:
-                    //1. 将此处的跳帧封装成单独的递归方法;
-                    //2. 下面传给四模式的代码,用bool方式直接返回finish的判断不妥,改之;
-                    
-                    
-                    
-                }else{
-                    ELog(@"如打出此错误,则查下为何groupModel不是TOFoModel类型,因为一般行为化的都是概念,而概念的父级就是TOFoModel");
-                }
-                
-                
-                
-            }
-            
-        }
         
     }
     
@@ -277,6 +238,86 @@
         return [self.delegate aiThinkOutPercept_EnergyValid];
     }
     return false;
+}
+
+/**
+ *  MARK:--------------------"外层输出" 推进 "中层循环"--------------------
+ */
+-(void) outerPushMiddleLoop:(DemandModel*)demand latestMatchAlg:(AIAlgNodeBase*)latestMatchAlg{
+    if (!latestMatchAlg) {
+        return;
+    }
+    
+    //2. 优先最新一帧,与上轮循环做匹配 (对单帧Finish的,要在下轮input传回判断是否符合要求,并跳转至下帧);
+    
+    //a. 输出行为的actYes要进行下轮匹配,比如吃,确定自己是否真吃了;
+    //b. 未输出行为,等待中的,也要进行下轮匹配,比如等开饭,等来开饭了; (等待的status是ActNo还是Runing?)
+    
+    
+    
+    
+    //2. 优先最新一帧,与上轮循环做匹配 (对单帧Finish的,要在下轮input传回判断是否符合要求,并跳转至下帧);
+    
+    //a. 输出行为的actYes要进行下轮匹配,比如吃,确定自己是否真吃了;
+    //b. 未输出行为,等待中的,也要进行下轮匹配,比如等开饭,等来开饭了; (等待的status是ActNo还是Runing?)
+    
+    //b. 取出所有等待下轮的outModel;
+    NSArray *waitModels = [TOUtils getSubOutModels_AllDeep:demand validStatus:@[@(TOModelStatus_ActYes),@(TOModelStatus_Runing)]];
+    
+    //c. 判断最近一次input是否与等待中outModel相匹配;
+    for (TOModelBase *waitModel in waitModels) {
+        if ([TOUtils mIsC_1:latestMatchAlg.pointer c:waitModel.content_p]) {
+            
+            //d. 匹配,则完成;
+            waitModel.status = TOModelStatus_Finish;
+            
+            //TODOTOMORROW:
+            //4. 此处在for循环中,所以有可能推进多条?
+            [self outModelLoopBack:waitModel];
+        }
+    }
+}
+
+/**
+ *  MARK:--------------------新发生outModel完成,推进递归--------------------
+ *  @desc
+ *      1. 本方法,以递归方式运行;
+ *      2. 最终输出为:
+ *          a. 时序下帧概念
+ *          b. 概念下帧稀疏码
+ *          c. Demand最终完成
+ */
+-(void) outModelLoopBack:(TOModelBase*)newFinishModel {
+    //1. 数据检查
+    if (!newFinishModel) {
+        return;
+    }
+    
+    //TODOTOMORROW:
+    //e. 并反馈给上一级,跳到下帧,
+    TOModelBase *groupModel = newFinishModel.baseOrGroup;
+    if (ISOK(groupModel, TOFoModel.class)) {
+        TOFoModel *toFoModel = (TOFoModel*)groupModel;
+        toFoModel.actionIndex ++;
+        [self.delegate aiTOP_2TOR_ReasonPlus:toFoModel];
+        
+        //f. foModel全完成了,则再递归至上一层;
+        TOModelBase *baseModel = toFoModel.baseOrGroup;
+        baseModel.status = TOModelStatus_Finish;
+        
+        //baseModel有可能是value也有可能是alg,再取group的话,有可能取到alg或fo;
+        //如果取到alg,则应将当前已完成的value标记到algOutModel.alreadyFinishs,并提给TOAction._P/_SP继续完成去;
+        //如果取到fo,则下帧继续;
+        
+        //TODOTOMORROW:
+        //1. 将此处的跳帧封装成单独的递归方法;
+        //2. 下面传给四模式的代码,用bool方式直接返回finish的判断不妥,改之;
+        
+        
+        
+    }else{
+        ELog(@"如打出此错误,则查下为何groupModel不是TOFoModel类型,因为一般行为化的都是概念,而概念的父级就是TOFoModel");
+    }
 }
 
 @end
