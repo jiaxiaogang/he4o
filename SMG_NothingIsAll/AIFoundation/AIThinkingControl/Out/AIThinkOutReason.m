8efc4011225515c2dc6ced5497595c7845b4c3bc
赤臂生 <jxg2764894@live.com>
2020-08-28 09:10:14 +0800

    写demand触发的反省类比 (未完成);

--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/AIThinkOutReason.m
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/AIThinkOutReason.m
@@ -22,6 +22,7 @@
 #import "TOValueModel.h"
 #import "DemandModel.h"
 #import "AITimeTrigger.h"
+#import "AIAbsAlgNode.h"
 
 @interface AIThinkOutReason() <TOActionDelegate>
 
@@ -734,43 +735,71 @@
             
         }else if(actYesModel.content_p.isOut){
             //2. 为行为输出时;
-            //向上找fo;
+            TOAlgModel *algModel = (TOAlgModel*)actYesModel;
+            TOFoModel *foModel = (TOFoModel*)algModel.baseOrGroup;
+            AIFoNodeBase *fo = [SMGUtils searchNode:foModel.content_p];
+            int algIndex = [fo.content_ps indexOfObject:algModel.content_p];
+            int deltaTime = [NUMTOOK(ARR_INDEX(fo.deltaTimes, algIndex)) intValue];
             
+            //b. 触发器
+            [AITimeTrigger setTimeTrigger:deltaTime canTrigger:^BOOL{
+                //c. 触发条件: (未等到实际输入);
+                return algModel.status == TOModelStatus_ActYes;
+            } trigger:^{
+                
+            }];
         }
     }else if(ISOK(actYesModel, TOFoModel.class)){
-        //a. 数据准备
+        //1. 数据准备
         TOFoModel *foModel = (TOFoModel*)actYesModel;
         AIFoNodeBase *actYesFo = [SMGUtils searchNode:foModel.content_p];
         DemandModel *demand = (DemandModel*)actYesModel.baseOrGroup;
+        AIFoNodeBase *foNode = [SMGUtils searchNode:foModel.content_p];
         
-        //b. 触发器
+        //2. 触发器
         [AITimeTrigger setTimeTrigger:actYesFo.mvDeltaTime canTrigger:^BOOL{
-            //c. 未等到实际输入 && 任务未在demandManager中抵消 -> 则触发;
-            return foModel.status != TOModelStatus_ActYes && demand.status != TOModelStatus_Finish;
+            //3. 触发条件: 任务未在demandManager中抵消;
+            return demand.status != TOModelStatus_Finish;
         } trigger:^{
-            //d. 触发反省类比_实际fo数据收集;
-            /////////对当前fo之下的,realContent_p进行收集,并组成实际realFo;
-            
-            
+            //4. 触发反省类比_实际fo数据收集 (不用收集realFo,而是直接对未修正部分构建,参考20205-原则1);
+            //@desc
+            //  1. 将每一帧中未被PM修正的稀疏码,构建成ATSubAlg;
+            //  2. 将所有帧ATSubAlg再连起来,构建成ATSubFo;
+            //  3. 根据subFo,从稀疏码向概念,再向时序索引查找,同样foNode的另外的assSubFo,并进行外类比;
+            //  4. 外类比构建更确切的S时序,如果已存在,则加强;
+            NSMutableArray *atSubFoContent = [[NSMutableArray alloc] init];
+            NSString *subDS = [ThinkingUtils getAnalogyTypeDS:ATSub];
             for (TOAlgModel *toAlgModel in foModel.subModels) {
                 if (ISOK(toAlgModel, TOAlgModel.class)) {
+                    ///1. 取到 "未修正稀疏码" (参考20205-原则2);
                     NSArray *except_ps = [TOUtils convertPointersFromTOValueModelSValue:toAlgModel.subModels invalidStatus:@[@(TOModelStatus_Finish)]];
                     NSArray *notFinish_ps = [SMGUtils removeSub_ps:except_ps parent_ps:toAlgModel.justPValues];
                     
+                    ///2. 未修正部分构建为: "ATSub概念"
+                    AIAlgNodeBase *curAlg = [SMGUtils searchNode:toAlgModel.content_p];
+                    if (!ARRISOK(notFinish_ps)) continue;
+                    AIAbsAlgNode *atSubAlg = [theNet createAbsAlg_NoRepeat:notFinish_ps conAlgs:@[curAlg] isMem:false ds:subDS];
                     
-                    //TODOTOMORROW: 20200828:
-                    //1. 对未完成的部分,进行构建概念;
-                    //2. 并收集起来,构建成S时序;
-                    
-                    //3. 并对同样别的S时序间,进行外类比,找出确切的S时序,如果已存在确切S时序,则加强;
-                    
-                    
-                    
-                    
+                    ///3. 收集ATSub概念_用于构建ATSub时序;
+                    [atSubFoContent addObject:atSubAlg.pointer];
                 }else{
                     WLog(@"查下此处,为何fo的subModel不是algModel类型,如果2020.10之前未见过此警告,可取消打印此日志;");
                 }
             }
+            
+            ///4. 构建ATSubFo
+            if (ARRISOK(atSubFoContent)) {
+                AINetAbsFoNode *subFo = [theNet createAbsFo_General:@[foNode] content_ps:atSubFoContent difStrong:1 ds:subDS];
+                
+                ///5. 向性左向右,以当前foNode为交集指引,找assSubFo,以进行外类比 (参考20205-原则3);
+                if (subFo) {
+                    
+                }
+                
+                
+                
+            }
+            
         }];
         
         //TODOTOMORROW:
