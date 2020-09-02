b30e16539a6a8f8eeaee57b4338ab643c84980ff
赤臂生 <jxg2764894@live.com>
2020-09-02 09:43:45 +0800

    TODOTOMORROW: 将反省结果应用于PM理性评价;

--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/AIThinkOutReason.m
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/AIThinkOutReason.m
@@ -431,6 +431,17 @@
     NSArray *sameLevelAlg_ps = [SMGUtils convertPointersFromPorts:[AINetUtils conPorts_All:M]];
     BOOL firstPNeedGL = true;
     if (firstJustPValue) {
+        
+        
+        //TODOTOMORROW: 20200903-将反省结果应用于PM理性评价
+        //1. outModel.baseOrGroup是CAlg
+        //      如: A22(速0,高5,距0,向→,皮0)
+        //2. 而outModel.base.base是CFo (即当前解决方案)
+        //      如: P+新增一例解决方案: F23[A22(速0,高5,距0,向→,皮0),A1(吃1)]->M7{64};
+        //3. 对CFo取ATSub抽象,看是否自省长过教训;
+        //4. 或者CFo中,没有距>0的概念,所以查下是否对预期Fo将具象概念替换进去,然后再形成时序和ATSub抽象指向;
+        
+        
         //a. 取出首个独特稀疏码,从同层概念中,获取模糊序列 (根据pValue_p对sameLevel_ps排序) (因为性能仅排20条);
         NSArray *sortAlgs = [ThinkingUtils getFuzzySortWithMaskValue:firstJustPValue fromProto_ps:ARR_SUB(sameLevelAlg_ps, 0, 20)];
         
