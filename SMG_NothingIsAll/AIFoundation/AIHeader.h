1ca97edb67d8af9393819306ac035ab6ad9719f3
赤臂生 <jxg2764894@live.com>
2020-10-19 09:45:06 +0800

    none

--- SMG_NothingIsAll/AIFoundation/AIHeader.h
+++ SMG_NothingIsAll/AIFoundation/AIHeader.h
@@ -211,9 +211,9 @@
 //识别时序
 #define Log4MFo false
 //内类比
-#define Log4InAna false
+#define Log4InAna true
 #define Log4InOutAna false
-#define Log4InAnaGL false
+#define Log4InAnaGL true
 #define Log4InAnaHN false
 //正向类比
 #define Log4SameAna true
