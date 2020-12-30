59207ac460fe516f0f6e7aa4d200ce5c19c00c1a
赤臂生 <jxg2764894@live.com>
2020-12-30 22:18:13 +0800

    继续写值域求和 (未完成);

--- SMG_NothingIsAll/AIFoundation/AIClass/Utils/SMGUtils+Sum.h
+++ SMG_NothingIsAll/AIFoundation/AIClass/Utils/SMGUtils+Sum.h
@@ -14,4 +14,28 @@
  */
 @interface SMGUtils (Sum)
 
+/**
+ *  MARK:--------------------值域求和--------------------
+ */
++(NSArray*) sumSPorts:(NSArray*)sPorts pPorts:(NSArray*)pPorts;
+
+/**
+ *  MARK:--------------------判断value处在S还是P中--------------------
+ */
++(AnalogyType) checkValueSPType:(double)value sumSPModel:(NSArray*)sumSPModel;
+
+@end
+
+
+//MARK:===============================================================
+//MARK:                     < SumModel单点模型 >
+//MARK:===============================================================
+@interface SumModel : NSObject
+
+//交点值
+@property (assign, nonatomic) double dotValue;
+
+//向右的方向类型(S/P)
+@property (assign, nonatomic) AnalogyType type;
+
 @end
--- SMG_NothingIsAll/AIFoundation/AIClass/Utils/SMGUtils+Sum.m
+++ SMG_NothingIsAll/AIFoundation/AIClass/Utils/SMGUtils+Sum.m
@@ -14,48 +14,66 @@
 
 /**
  *  MARK:--------------------值域求和--------------------
- *  @desc 波求和,不过采取线函数来替代找交点 (参考21212 & 21213);
+ *  @desc 束波求和简化版,采取线函数来替代找交点 (参考21212 & 21213);
+ *  @result Array[SumModel] notnull
  */
-+(void) sumPortsA:(NSArray*)as portsB:(NSArray*)bs{
++(NSArray*) sumSPorts:(NSArray*)sPorts pPorts:(NSArray*)pPorts{
     //1. 数据检查;
-    as = ARRTOOK(as);
-    bs = ARRTOOK(bs);
-    NSArray *alls = [SMGUtils collectArrA:as arrB:bs];
+    sPorts = ARRTOOK(sPorts);
+    pPorts = ARRTOOK(pPorts);
+    NSArray *allPorts = [SMGUtils collectArrA:sPorts arrB:pPorts];
+    NSMutableArray *result = [[NSMutableArray alloc] init];
     
     //2. 从小到大排序;
-    [alls sortedArrayUsingComparator:^NSComparisonResult(AIPort *p1, AIPort *p2) {
+    [allPorts sortedArrayUsingComparator:^NSComparisonResult(AIPort *p1, AIPort *p2) {
         double v1 = [NUMTOOK([AINetIndex getData:p1.target_p]) doubleValue];
         double v2 = [NUMTOOK([AINetIndex getData:p2.target_p]) doubleValue];
         return [SMGUtils compareFloatA:v2 floatB:v1];
     }];
     
-    
-    
-    
-}
-
-
-+(NSArray*) getFuzzySortWithMaskValue:(AIKVPointer*)maskValue_p fromProto_ps:(NSArray*)proto_ps{
-    //a. 对result2筛选出包含同标识value值的: result3;
-    __block NSMutableArray *validConDatas = [[NSMutableArray alloc] init];
-
-    //b. 对result3进行取值value并排序: result4 (根据差的绝对值小的排前面);
-    double pValue = [NUMTOOK([AINetIndex getData:maskValue_p]) doubleValue];
-    NSArray *sortConDatas = [validConDatas sortedArrayUsingComparator:^NSComparisonResult(NSDictionary *obj1, NSDictionary *obj2) {
-        double v1 = [NUMTOOK([obj1 objectForKey:@"v"]) doubleValue];
-        double v2 = [NUMTOOK([obj2 objectForKey:@"v"]) doubleValue];
-        double absV1 = fabs(v1 - pValue);
-        double absV2 = fabs(v2 - pValue);
-        return absV1 > absV2 ? NSOrderedDescending : absV1 < absV2 ? NSOrderedAscending : NSOrderedSame;
-    }];
-    
-    //c. 转成sortConAlgs
-    NSMutableArray *result = [[NSMutableArray alloc] init];
-    for (NSDictionary *sortConData in sortConDatas) {
-        AIAlgNodeBase *algNode = [sortConData objectForKey:@"a"];
-        [result addObject:algNode];
+    //3. 取上组和当前组;
+    NSMutableArray *lastGroup = [[NSMutableArray alloc] init];
+    NSMutableArray *curGroup = [[NSMutableArray alloc] init];
+    AnalogyType curType = ATNone;
+    for (AIPort *item in allPorts) {
+        //4. 切换组: (收集中!=None & 非同组!=itemType);
+        AnalogyType itemType = [sPorts containsObject:item] ? ATSub : ATPlus;
+        if (curType != ATNone && curType != itemType) {
+            //5. 切组后: curGroup与lastGroup进行对比找出各自最有竞争力的成员;
+            
+            //7. 切组后: 根据最有竞争力成员算出交点;
+            
+            //8. 生成模型,并收集;
+            SumModel *model = [[SumModel alloc] init];
+            model.dotValue = 0;
+            model.type = curType;
+            [result addObject:model];
+            
+            //9. 当前组变成上组 (当前组重置);
+            [lastGroup removeAllObjects];
+            [lastGroup addObjectsFromArray:curGroup];
+            [curGroup removeAllObjects];
+            
+        }
+        //10. 收集curGroup: (不切组 或 切完组);
+        [curGroup addObject:item];
+        curType = itemType;
     }
     return result;
+}
+
+/**
+ *  MARK:--------------------判断value处在S还是P中--------------------
+ */
++(AnalogyType) checkValueSPType:(double)value sumSPModel:(NSArray*)sumSPModel{
+    //1. 数据准备;
+    sumSPModel = ARRTOOK(sumSPModel);
+    
+    //2. 从中找value匹配;
+    for (SumModel *item in sumSPModel) {
+        
+    }
+    return ATNone;
 }
 
 @end
