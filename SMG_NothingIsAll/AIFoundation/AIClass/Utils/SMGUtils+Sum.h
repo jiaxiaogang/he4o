7dc02b99b64a02468bd4d6ca180ff6b19ef936a4
赤臂生 <jxg2764894@live.com>
2020-12-30 08:38:35 +0800

    写值域求和 (SMGUtils+Sum) (未完成);

---
+++ SMG_NothingIsAll/AIFoundation/AIClass/Utils/SMGUtils+Sum.h
@@ -0,0 +1,17 @@
+//
+//  SMGUtils+Sum.h
+//  SMG_NothingIsAll
+//
+//  Created by jia on 2020/12/30.
+//  Copyright © 2020年 XiaoGang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ *  MARK:--------------------求和工具类--------------------
+ *  @desc 从SMGUtils单独出来,因为Sum比较麻烦,得支持值域求和 (参考n21p21);
+ */
+@interface SMGUtils (Sum)
+
+@end
---
+++ SMG_NothingIsAll/AIFoundation/AIClass/Utils/SMGUtils+Sum.m
@@ -0,0 +1,13 @@
+//
+//  SMGUtils+Sum.m
+//  SMG_NothingIsAll
+//
+//  Created by jia on 2020/12/30.
+//  Copyright © 2020年 XiaoGang. All rights reserved.
+//
+
+#import "SMGUtils+Sum.h"
+
+@implementation SMGUtils (Sum)
+
+@end
