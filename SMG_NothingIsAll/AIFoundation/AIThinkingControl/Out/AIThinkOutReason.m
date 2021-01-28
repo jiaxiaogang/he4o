c411cfd02275e93d6f421023fcb669aec9dc1191
赤臂生 <jxg2764894@live.com>
2021-01-28 08:45:24 +0800

    恢复R-模式在ActYes触发Out反省 (参考22082);

--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/AIThinkOutReason.m
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/AIThinkOutReason.m
@@ -729,7 +729,7 @@
 /**
  *  MARK:--------------------ActYes的流程控制--------------------
  *  @desc : 预测tor_Forecast: 当ActYes时,一般等待外循环反馈,而此处构建生物钟触发器,用于超时时触发反省类比;
- *      1. 调用AITime触发器;
+ *      1. 调用AITime触发器 (为了Out反省);
  *      2. 当生物钟触发器触发时,如果未输入有效"理性推进" 或 "感性抵消",则对这些期望与实际的差距进行反省类比;
  *  @callers
  *      1. demand.ActYes处
@@ -740,7 +740,8 @@
  *  @version
  *      2020.10.17: 在生物钟触发器触发器,做有根判定,任务失效时,不进行反省 (参考note21-todolist-1);
  *      2020.12.18: HNGL失败时再调用Begin会死循环的问题,改为HNGL.ActYes失败时,则直接调用FC.Failure(hnglAlg);
- *      2021.01.27: R-模式的ActYes仅赋值,不在此处做触发器 (参考22076-todo4);
+ *      2021.01.27: R-模式的ActYes仅赋值,不在此处做触发器 (参考22081-todo4);
+ *      2021.01.28: R-模式的ActYes在此处触发Out反省,与昨天思考的In反省触发不冲突 (参考22082);
  */
 -(void) singleLoopBackWithActYes:(TOModelBase*)actYesModel {
     NSLog(@"\n\n=============================== 流程控制:ActYes ===============================\nModel:%@ %@",actYesModel.class,Pit2FStr(actYesModel.content_p));
@@ -792,7 +793,32 @@
             //1. R-模式ActYes处理,仅赋值,等待R-触发器;
             ReasonDemandModel *demand = (ReasonDemandModel*)actYesModel.baseOrGroup;
             demand.status = TOModelStatus_ActYes;
-            NSLog(@"---//触发器R-_任务:%@ 破壁完成:%@",Fo2FStr(demand.mModel.matchFo),Pit2FStr(actYesModel.content_p));
+            
+            //2. 取matchFo已发生,到末位mvDeltaTime,所有时间之和做触发;
+            AIFoNodeBase *matchFo = demand.mModel.matchFo;
+            double deltaTime = [TOUtils getSumDeltaTime2Mv:matchFo cutIndex:demand.mModel.cutIndex];
+            
+            //3. 触发器;
+            NSLog(@"---//触发器R-_任务:%@ 破壁:%@ time:%f",Fo2FStr(matchFo),Pit2FStr(actYesModel.content_p),deltaTime);
+            [AITime setTimeTrigger:deltaTime trigger:^{
+                
+                //3. 反省类比 (当OutBack发生,则破壁失败S,否则成功P);
+                AnalogyType type = (actYesModel.status == TOModelStatus_OuterBack) ? ATSub : ATPlus;
+                NSLog(@"---//触发器R-_任务:%@ 破壁:%@ (%@)",Fo2FStr(matchFo),Pit2FStr(actYesModel.content_p),ATType2Str(type));
+                
+                //4. 暂不开通反省类比,等做兼容PM后,再打开反省类比;
+                [AIAnalogy analogy_ReasonRethink:(TOFoModel*)actYesModel cutIndex:NSIntegerMax type:type];
+                
+                //4. 失败时,转流程控制-失败 (会开始下一解决方案) (参考22061-8);
+                if (type == ATSub) {
+                    actYesModel.status = TOModelStatus_ScoreNo;
+                    [self singleLoopBackWithFailureModel:actYesModel];
+                }else{
+                    //5. SFo破壁成功,完成任务 (参考22061-9);
+                    actYesModel.status = TOModelStatus_Finish;
+                    [theTC.outModelManager removeDemand:demand];
+                }
+            }];
             return;
         }
         
