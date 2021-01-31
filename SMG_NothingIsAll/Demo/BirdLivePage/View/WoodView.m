19c39598fa7063ac38c6a4e1f3cb87461c8f47ef
赤臂生 <jxg2764894@live.com>
2021-01-31 18:34:10 +0800

    回归防撞训练测得22112BUG;

--- SMG_NothingIsAll/Demo/BirdLivePage/View/WoodView.m
+++ SMG_NothingIsAll/Demo/BirdLivePage/View/WoodView.m
@@ -21,7 +21,7 @@
 
 -(void) initView{
     //self
-    [self setFrame:CGRectMake(0, ScreenHeight - 150 - 64 - 20, 5, 150)];
+    [self setFrame:CGRectMake(0, ScreenHeight - 100 - 64 - 20, 5, 100)];
     [self setBackgroundColor:UIColorWithRGBHex(0x825528)];
 }
 
