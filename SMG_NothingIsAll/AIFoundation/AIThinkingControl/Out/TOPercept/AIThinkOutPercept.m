9a41fde4fe78df3fb93eb47611d252d230f36264
赤臂生 <jxg2764894@live.com>
2021-02-04 20:16:43 +0800

    修复22115BUG-R-模式越挫越勇的问题;

--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/TOPercept/AIThinkOutPercept.m
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/TOPercept/AIThinkOutPercept.m
@@ -126,6 +126,8 @@
 /**
  *  MARK:--------------------"外层输入" 推进 "中层循环" 决策--------------------
  *  @title 外层输入对Out短时记忆的ReasonDemandModel影响处理 (参考22061-8);
+ *  @version
+ *      2021.02.04: 将R同区同向(会导致永远为false因为虚mv得分为0)判断,改为同区反向判断 (参考22115BUG & 22108虚mv反馈判断方法);
  */
 +(void) top_OPushM:(AICMVNodeBase*)newMv{
     //1. 数据检查
@@ -137,9 +139,8 @@
     for (ReasonDemandModel *demand in demands) {
         if (!ISOK(demand, ReasonDemandModel.class)) continue;
         
-        //3. 判断hope(wait)和real(new)之间是否相符 (与newMv同区且同向) (匹配,比如撞疼,确定疼了);
-        BOOL isSame = [AIScore sameIdenSameScore:demand.mModel.matchFo.cmvNode_p mv2:newMv.pointer];
-        if (!isSame) continue;
+        //3. 判断hope(wait)和real(new)之间是否相符 (当反馈了"同区反向"时,即表明任务失败,为S) (匹配,比如撞疼,确定疼了);
+        if ([AIScore sameIdenDiffDelta:demand.mModel.matchFo.cmvNode_p mv2:newMv.pointer]) continue;
         
         //4. 将等待中的foModel改为OutBack;
         for (TOFoModel *foModel in demand.actionFoModels) {
