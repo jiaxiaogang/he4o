9c568ea6ddf8af0b99fb75c4db055a570a268398
赤臂生 <jxg2764894@live.com>
2021-03-05 09:39:05 +0800

    Action._Hav迭代代码规划 (参考22152);

--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/TOReason/AIThinkOutAction.m
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/TOReason/AIThinkOutAction.m
@@ -220,6 +220,19 @@
         //1. mv0任务在mIsC找不到匹配的alg时;
         //2. 不可贸然进行cHav,以免弄巧成拙,反把老虎放出来;
         
+        
+        //TODOTOMORROW20210305: R-模式_Hav首先是为了避免forecastAlg,其次才是为了达成curFo解决方案;
+        //1. 判断当前是R-模式;
+        //2. 判断在RDemand.forecastFo中,cutIndex之后是否有和curAlg共同的抽象或本身就是抽具象关系的forecastAlg;
+        //  a. 如果有,则直接ActYes,等待其自然出现 (疑惑,比如主动视觉: 想知道附近有没危险,要主动去探看);
+        //      1> Outback返回,则会再调用到此处_Hav并调用第2级,PM进行修正处理;
+        //      2> OutBack未返回,静默成功;
+        //  b. 如果无,则继续cHav;
+        
+        
+        
+        
+        
         //5. 去掉不应期
         NSArray *except_ps = [TOUtils convertPointersFromTOModels:outModel.actionFoModels];
         
