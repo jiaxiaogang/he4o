44822ebd491b8e4d2c1f64530d9aa5376cc735b5
赤臂生 <jxg2764894@live.com>
2021-06-29 09:27:15 +0800

    查23152-抽象dsFo被不应期掉的原因 (发现反思的已发生cutIndex定义不清晰);

--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/In/Model/AIMatchFoModel.h
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/In/Model/AIMatchFoModel.h
@@ -10,6 +10,9 @@
 
 /**
  *  MARK:--------------------单条matchFo模型--------------------
+ *  @version
+ *      2021.06.29: 对于认知识别时matchFo而言,匹配cutIndex与已发生actionIndex是一回事;
+ *      2021.06.29: 对于反思时matchFo而言,匹配cutIndex与已发生actionIndex并不是一回事 (因为反思是"假设"这么做,会怎么样);
  */
 @class AIFoNodeBase;
 @interface AIMatchFoModel : NSObject
@@ -17,7 +20,8 @@
 +(AIMatchFoModel*) newWithMatchFo:(AIFoNodeBase*)matchFo matchFoValue:(CGFloat)matchFoValue cutIndex:(NSInteger)cutIndex;
 @property (strong, nonatomic) AIFoNodeBase *matchFo;    //匹配时序
 @property (assign, nonatomic) CGFloat matchFoValue;     //时序匹配度
-@property (assign, nonatomic) NSInteger cutIndex;       //已发生与预测的截点 (0开始,已发生含cutIndex);
+@property (assign, nonatomic) NSInteger cutIndex;       //proto在match中匹配到的最后一位,在match中的下标;
+@property (assign, nonatomic) NSInteger actionIndex;    //已发生与预测的截点 (0开始,已发生含cutIndex);
 @property (assign, nonatomic) TIModelStatus status;     //状态
 
 @end
