df2ea0e5e5e0db961be5c3f0645849aaf2f27af1
赤臂生 <jxg2764894@live.com>
2021-07-07 09:45:05 +0800

    反思时,cutIndex全部返-1 (参考23156);

--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/In/TIReason/AIThinkInReason.m
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/In/TIReason/AIThinkInReason.m
@@ -211,6 +211,7 @@
  *      2020.04.03 : 将assFoIndexAlg由proto.lastIndex改为replaceMatchAlg来代替 (因为lastAlg索引失败率太高);
  *      2020.07.17 : 换上新版partMatching_FoV2时序识别算法;
  *      2021.04.13 : 将装饰AIShortMatchModel改为result返回 & 参数由order直接改为fo传入;
+ *      2021.07.07 : 反思时,cutIndex全部返-1 (参考23156);
  *  @todo :
  *      2020.04.03: 支持识别到多个时序 T;
  *      2020.04.03: 以识别到的多个时序,得到多个价值预测 (支持更多元的评价);
@@ -224,41 +225,6 @@
     
     //2. 调用通用时序识别方法 (checkItemValid: 可考虑写个isBasedNode()判断,因protoAlg可里氏替换,目前仅支持后两层)
     [self partMatching_FoV1Dot5:fo except_ps:@[fo.pointer] decoratorInModel:result findCutIndex:^NSInteger(AIFoNodeBase *matchFo, NSInteger lastMatchIndex) {
-        
-        //TODOTOMORROW20210705: 从父级的0-cutIndex 向子级的0-lastMatchIndex 找最后匹配截点cutIndex;
-        
-        //需要把有效范围传给TIR_Fo_CheckFoValidMatch
-        //即lastMatchIndex和baseDemand.mModel.cutIndex2
-        
-        //反思: 此处subFo和baseFo相距甚远,甚至没任何关系,我们需要举更多示例,来分析更紧密的"已发生截点"传递机制;
-        //1. 主任务: 快乐;
-        //      > dsFo: 穿越森林
-        //      > 反思: 有可能遇虎,有危险 (已发生源于反思假想);
-        //2. 子任务: 解决危险;
-        //      > dsFo: 举枪打虎
-        //      > cutIndex: 现在还没老虎出现;
-        
-        //1. 主任务: 快乐;
-        //      > dsFo: 穿越森林
-        //      > 反思: 看到前边有老虎 (已发生源于识别预测);
-        //2. 子任务: 解决危险;
-        //      > dsFo: 捡石头吓跑它
-        //      > cutIndex: 现在已有老虎出现;
-        
-        //1. 主任务: 快乐;
-        //      > dsFo: 穿越森林
-        //      > 反思: 有可能遇虎,有危险 (已发生源于反思假想);
-        //2. 子任务: 解决危险;
-        //      > dsFo: 遇虎时大喊滚开
-        //      > cutIndex: ;
-        
-        //综上: 无论怎么分析,反思时已发生部分全是-1,后续真实发生时,预测,此时就一定不再是-1了;
-        
-        AIFoNodeBase *baseFo = baseDemand.mModel.matchFo;
-        AIFoNodeBase *subFo = matchFo;
-        [TIRUtils TIR_Fo_CheckFoValidMatch:baseFo assFo:subFo success:^(NSInteger lastAssIndex, CGFloat matchValue) {
-            NSInteger cutIndex = lastAssIndex;
-        }];
         return -1;
     }];
     NSLog(@"反思时序: Finish >> %@",Fo2FStr(result.matchFo));
