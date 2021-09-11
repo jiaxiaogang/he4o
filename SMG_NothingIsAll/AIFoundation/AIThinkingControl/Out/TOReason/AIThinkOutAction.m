dba10f91975d58438a5f792009171ab3f04dba27
赤臂生 <jxg2764894@live.com>
2021-09-11 17:16:23 +0800

    启用内中外类比v5 (未测试)

--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/TOReason/AIThinkOutAction.m
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/TOReason/AIThinkOutAction.m
@@ -335,7 +335,7 @@
     
     //2. 根据type和value_p找ATLess/ATGreater
     //3. 根据havAlg联想时序,并找出新的解决方案,与新的行为化的概念,与新的条件概念; (2020.11.06: 由getInner1Alg直接取relativeFos);
-    AIKVPointer *relativeFo_p = [AINetService getInnerV3_GL:baseAlg.pm_InModel vAT:vAT vDS:vDS type:type except_ps:except_ps];
+    AIKVPointer *relativeFo_p = [AINetService getInnerV4_GL:baseAlg.pm_InModel vAT:vAT vDS:vDS type:type except_ps:except_ps];
     if (Log4ActGL) NSLog(@"getInnerAlg(%@): 根据:%@->%@ 找:%@%@ \n联想结果:%@ %@",ATType2Str(type),Pit2FStr(outModel.sValue_p),Pit2FStr(outModel.content_p),vDS,Data2FStr(type, vAT, vDS),Pit2FStr(relativeFo_p),relativeFo_p ? @"↓↓↓↓↓↓↓↓" : @"无计可施");
     
     //5. 转移至_fos
