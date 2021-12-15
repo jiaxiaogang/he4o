f870d2c960cb8540b3d7f5d12ca79b1cd2c20e08
赤臂生 <jxg2764894@live.com>
2021-12-16 07:17:51 +0800

    写短时记忆树综合评分 (完成)

--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/TCPlan/TCPlan.m
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/TCPlan/TCPlan.m
@@ -57,6 +57,29 @@
     
     
     
+    //TODOTOMORROW20211216:
+    //方案1:
+    //1. 把任务树捋顺,有几个root根取出来;
+    //2. 写个PlanFo虚foModel,把所有root根挂在它下面;
+    //3. 到getPlanSolution取出来最优路径,执行之;
+    //  a. 从rootFo.sub出发取出RootDemands;
+    //  b. 判断rootDemand和其下S的评分对比,做感性淘汰;
+    //  c. 再找出最高分的路径,进行行为化;
+    
+    //方案2:
+    //1. 从任务树取出几个root;
+    //2. 对每个root,分别进行single综合评分;
+    //  a. root无解决方案时,rootDemand自身就是迫切度评分;
+    //  b. root有解决方案时,分别对root的解决方案评分找出最优方案;
+    //  c. 对每个root各自判断最优方案是否感性淘汰;
+    //  d. 没淘汰的root们,竞争哪个更有效,执行之;
+    
+    //方案3:
+    //1. 从任务树取出几个root;
+    //2. 对每个root,直接进行迫切度竞争,取出老大进行行为化;
+    //  a. 对firstRoot进行single综合评分,并取最优路径;
+    //  b. 判断最优路径是否感性淘汰->淘汰的话继续下一个secondRoot,尝试第a步;
+    //  c. 判断最优路径是否感性淘汰->未淘汰的话继续行为化;
     
     
     
@@ -68,23 +91,117 @@
 
 /**
  *  MARK:--------------------对toModel综合评分--------------------
- *  @desc 对解决方案S进行综合评分;
+ *  @desc 对解决方案S进行综合评分 (参考24192);
  *  @result 将rootModel及其下分枝评分全部计算,并存为字典返回 <K=foModel,V=score>;
  */
-+(NSDictionary*) score4Solution:(TOFoModel*)model{
++(NSArray*) getPlanSolution:(TOFoModel*)rootFo{
     //1. 数据准备;
-    NSMutableDictionary *result = [[NSMutableDictionary alloc] init];
+    NSMutableDictionary *scoreDic = [[NSMutableDictionary alloc] init];
     
-    //2. 从model向下取subModels,遇到R就求和,遇到S就竞争;
+    //2. 算出得分字典;
+    [self score4Solution_Single:rootFo scoreDic:scoreDic];
     
-    model.subDemands
+    //3. 得出最优路径;
+    NSArray *bestWay = [self bestWay:scoreDic rootFo:rootFo];
+    return bestWay;
+}
+
+/**
+ *  MARK:--------------------短时记忆树综合评分--------------------
+ *  @desc
+ *      1. 缩写说明: 1.sr=SubRDemand 2.ss=SubSolution 3.sa=SubAlgModel 4.sh=SubHDemand
+ *      2. 每执行一次single方法,则scoreDic中收集一条model的得分 <foModel,score>;
+ *      3. S竞争方法由_Best方法实现;
+ *      4. R求和方法主要在_Single中实现;
+ *      5. 先将所有得分算完后,再重新从root开始算最优路径,因为只有子枝算完,父枝才能知道怎么算最优路径;
+ */
++(void) score4Solution_Single:(TOFoModel*)model scoreDic:(NSMutableDictionary*)scoreDic{
+    //1. 数据检查;
+    if (!scoreDic) scoreDic = [[NSMutableDictionary alloc] init];
+    double modelScore = 0;
     
-    //RDemand在FoModel.subDemands下; (R求和);
-    //HDemand在AlgModel.subDemands下; (H必须有解决方案,否则理性淘汰);
+    //===== 第一部分: RDemand在FoModel.subDemands下 (有解决方案:参与求和 & 无解决方案:理性淘汰);
+    //2. 用每个sa取sh子任务 (求和);
+    for (TOAlgModel *sa in model.subModels) {
+        
+        //3. 取出sh (一条sa最多只能生成一个sh任务);
+        HDemandModel *sh = ARR_INDEX(sa.subDemands, 0);
+        if (sh) {
+            //4. H有解决方案时,对S竞争,并将最高分计入modelScore;
+            if (ARRISOK(sh.actionFoModels)) {
+                
+                //a. 对S竞争;
+                TOFoModel *bestSS = [self score4Solution_Best:sh.actionFoModels scoreDic:scoreDic];
+                
+                //b. 将竞争胜者计入modelScore;
+                modelScore += [NUMTOOK([scoreDic objectForKey:bestSS.content_p]) doubleValue];
+            }else{
+                //5. H无解决方案时,则理性淘汰;
+                [scoreDic setObject:@(INT_MIN) forKey:model.content_p];
+                return;
+            }
+        }
+    }
     
+    //===== 第二部分: HDemand在AlgModel.subDemands下 (有解决方案:参与求和 & 无解决方案:R自身计入综合评分中);
+    //10. 取出subRDemands子任务 (求和);
+    for (ReasonDemandModel *sr in model.subDemands) {
+        
+        //11. R有解决方案时,对S竞争,并将最高分计入modelScore;
+        if (ARRISOK(sr.actionFoModels)) {
+            
+            //a. 对S竞争;
+            TOFoModel *bestSS = [self score4Solution_Best:sr.actionFoModels scoreDic:scoreDic];
+            
+            //b. 将竞争胜者计入modelScore;
+            modelScore += [NUMTOOK([scoreDic objectForKey:bestSS.content_p]) doubleValue];
+        }else{
+            //12. R无解决方案时,直接将sr评分计入modelScore;
+            double score = [AIScore score4MV:sr.algsType urgentTo:sr.urgentTo delta:sr.delta ratio:1.0f];
+            modelScore += score;
+        }
+    }
     
+    //13. 将求和得分,计入dic (当没有sr也没有sa子任务 = 0分);
+    [scoreDic setObject:@(modelScore) forKey:model.content_p];
+}
+
+/**
+ *  MARK:--------------------S解决方案竞争--------------------
+ *  @param foModels : 解决方案S数,必须>=1条;
+ *  @param scoreDic : notnull
+ *  @result 将bestFo返回;
+ */
++(TOFoModel*) score4Solution_Best:(NSArray*)foModels scoreDic:(NSMutableDictionary*)scoreDic{
+    //1. 取出子任务的每个解决方案S (竞争);
+    TOFoModel *bestFoModel = nil;
+    for (TOFoModel *foModel in foModels) {
+        
+        //2. 评分
+        [self score4Solution_Single:foModel scoreDic:scoreDic];
+        
+        //3. 竞争
+        if (!bestFoModel) {
+            bestFoModel = foModel;
+        }else{
+            double oldScore = [NUMTOOK([scoreDic objectForKey:bestFoModel.content_p]) doubleValue];
+            double newScore = [NUMTOOK([scoreDic objectForKey:foModel.content_p]) doubleValue];
+            if (newScore > oldScore) {
+                bestFoModel = foModel;
+            }
+        }
+    }
     
-    return result;
+    //4. 将最优S返回;
+    return bestFoModel;
+}
+
+/**
+ *  MARK:--------------------取最优路径--------------------
+ */
++(NSArray*) bestWay:(NSMutableDictionary*)scoreDic rootFo:(TOFoModel*)rootFo{
+
+    return nil;
 }
 
 +(void) plan:(DemandModel*)demand{
