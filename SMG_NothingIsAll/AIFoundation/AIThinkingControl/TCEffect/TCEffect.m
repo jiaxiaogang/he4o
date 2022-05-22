71b3089f5d728397029cd09eacffca813cec4fb7
赤臂生 <jxg2764894@live.com>
2022-05-22 13:15:00 +0800

    26095-写有效率 (未完成);

---
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/TCEffect/TCEffect.m
@@ -0,0 +1,21 @@
+//
+//  TCEffect.m
+//  SMG_NothingIsAll
+//
+//  Created by jia on 2022/5/22.
+//  Copyright © 2022年 XiaoGang. All rights reserved.
+//
+
+#import "TCEffect.h"
+
+@implementation TCEffect
+
++(void) rEffect:(ReasonDemandModel*)rDemand{
+    
+}
+
++(void) hEffect:(HDemandModel*)hDemand{
+    
+}
+
+@end
