20c89d68eb5ec811493edb224d22c9a2505d3664
jiaxiaogang <jxg2764894@live.com>
2023-03-07 19:02:13 +0800

    过滤器调整参数为: 结果16%,主辅力度比20:1;

--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/Score/AIFilter.m
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/Score/AIFilter.m
@@ -16,11 +16,11 @@
  *      2023.03.06: 概念识别过滤器匹配度为主,强度为辅 (参考28152-方案4-todo4);
  */
 +(NSArray*) recognitonAlgFilter:(NSArray*)matchAlgModels {
-    return [self filterTwice:matchAlgModels scoreBlock1:^double(AIMatchAlgModel *item) {
+    return [self filterTwice:matchAlgModels mainBlock:^double(AIMatchAlgModel *item) {
         return item.matchValue;
-    } rate1:0.2f scoreBlock2:^double(AIMatchAlgModel *item) {
+    } subBlock:^double(AIMatchAlgModel *item) {
         return item.strongValue;
-    } rate2:0.8f];
+    }];
 }
 
 /**
@@ -29,11 +29,11 @@
  *      2023.03.06: 时序识别过滤器强度为主,匹配度为辅 (参考28152-方案4-todo5);
  */
 +(NSArray*) recognitonFoFilter:(NSArray*)matchModels {
-    return [self filterTwice:matchModels scoreBlock1:^double(AIMatchFoModel *item) {
+    return [self filterTwice:matchModels mainBlock:^double(AIMatchFoModel *item) {
         return item.strongValue;
-    } rate1:0.2f scoreBlock2:^double(AIMatchFoModel *item) {
+    } subBlock:^double(AIMatchFoModel *item) {
         return item.matchFoValue;
-    } rate2:0.8];
+    }];
 }
 
 //MARK:===============================================================
@@ -42,35 +42,54 @@
 
 /**
  *  MARK:--------------------同时符合两项过滤器的前xx% (参考28152-方案3)--------------------
+ *  @desc 公式说明:
+ *      1. 要求: 总过滤数20 = 总数30 - 结果数10;
+ *      2. 主辅任务力度: 等于4:1时: 主过滤掉16条,辅过滤掉4条 即可;
+ *      3. 主辅过滤条数: 主过滤后,剩下14(30-16)条; 辅过滤后剩下10(14-4)条;
+ *      4. 主辅过滤率: "主过滤率 = 剩下14 / 总数30","辅过滤率 = 剩下10 / 剩下14";
+ *      5. 最终成功留下结果10条;
+ *  @desc 可配置项 (注:每项数字可调):
+ *      1. 结果数: 越大返回越多;
+ *      2. 主辅任务比例: 越大主过滤器作用越大;
+ *      3. 最小条数百分比: 值越小越准;
+ *  @desc 现配置: 结果数为16%,主辅过滤力度20:1,即主过滤掉80%,辅再过滤掉剩下的20%;
+ *
  *  @version
  *      2023.03.06: 过滤前20%改为35% (参考28152-方案3-todo2);
  *      2023.03.07: 减少过滤结果条数(从10到3),避免过滤器久久不生效 (参考28152b-todo1);
  *      2023.03.07: 过滤率改成动态计算,使其条数少时,两个过滤器也都能生效 (参考28152b-todo2);
  *      2023.03.07: 修改主辅过滤器为嵌套执行 (参考28152b-todo3);
+ *      2023.03.07: 结果保留改为16%,将主辅力度调整为20:1 (因为实测4:1时,真实主过滤率=37%左右,太高了);
  */
-+(NSArray*) filterTwice:(NSArray*)protoArr scoreBlock1:(double(^)(id item))scoreBlock1 rate1:(CGFloat)rate1 scoreBlock2:(double(^)(id item))scoreBlock2 rate2:(CGFloat)rate2{
++(NSArray*) filterTwice:(NSArray*)protoArr mainBlock:(double(^)(id item))mainBlock subBlock:(double(^)(id item))subBlock {
     //0. 数据准备;
     if (!ARRISOK(protoArr)) return protoArr;
-    //公式说明:
-    //1. 要求: 总过滤数20 = 总数30 - 结果数10;
-    //2. 主过滤0.8,辅过滤0.2时: 主过滤掉16条,辅过滤掉4条 即可;
-    //3. 所以: 主过滤后,剩下14(30-16)条; 辅过滤后剩下10(14-4)条;
-    //3. 所以: 主过滤率 = 剩下14 / 总数30;
-    //4. 辅过滤率 = 剩下10 / 剩下14;
-    NSInteger protoCount = protoArr.count;                          //总数30;
+    
+    //1. 条数 (参考注释公式说明-1);
+    NSInteger protoCount = protoArr.count;                          //总数 (如30);
+    CGFloat minResultNum = 0.16f * protoCount;                      //最小条数 (建议16%,值越小越准);
     NSInteger resultNum = 3;                                        //结果返回至少2条;
-    resultNum = MAX(protoCount * 0.2f, MIN(resultNum, protoCount)); //结果需要 大于20% 且 小于100%;
-    NSInteger filterNum = protoCount - resultNum;                   //总过滤任务 (比如共30条,剩10条,过滤任务就是70%);
-    CGFloat totalFilterForce = rate2 / rate1 + 1;                   //总过滤力量份数 (比如主为0.2,辅为0.8,则总力量=5份);
-    CGFloat fuFilterNum = filterNum / totalFilterForce;             //辅过滤条数;
+    resultNum = MAX(minResultNum, MIN(resultNum, protoCount));      //结果需要 大于20% 且 小于100%;
+    
+    //2. 过滤任务和力度 (参考注释公式说明-2);
+    NSInteger filterNum = protoCount - resultNum;                   //总过滤任务 (比如共30条,剩10条,过滤任务就是20条);
+    CGFloat zuFilterForce = 20, fuFilterForce = 1;                  //主辅两过滤器的力度权重 (一般主力度要大于辅力度多倍);
+    CGFloat totalForce = zuFilterForce + fuFilterForce;             //总过滤力量份数 (比如: 主4 + 辅1 = 总力5份);
+    
+    //3. 主辅过滤条数 (参考注释公式说明-3);
+    CGFloat fuFilterNum = filterNum / totalForce * fuFilterForce;   //辅过滤条数;
     CGFloat zuFilterNum = filterNum - fuFilterNum;                  //主过滤条数;
+    
+    //4. 主辅过滤率 (参考注释公式说明-4);
     CGFloat zuRate = (protoCount - zuFilterNum) / protoCount;       //主过滤率;
     CGFloat fuRate = resultNum / (protoCount - zuFilterNum);        //辅过滤率;
     
     //5. 主中辅,嵌套过滤 (参考28152b-todo3);
-    NSArray *filter1 = ARR_SUB([SMGUtils sortBig2Small:protoArr compareBlock:scoreBlock1], 0, protoArr.count * zuRate);
-    NSArray *filter2 = ARR_SUB([SMGUtils sortBig2Small:filter1 compareBlock:scoreBlock2], 0, filter1.count * fuRate);
-    NSLog(@"过滤器: 总%ld需%ld 主:%.2f(剩:%ld) 辅:%.2f(剩:%ld)",protoCount,resultNum,zuRate,filter1.count,fuRate,filter2.count);
+    NSArray *filter1 = ARR_SUB([SMGUtils sortBig2Small:protoArr compareBlock:mainBlock], 0, protoArr.count * zuRate);
+    NSArray *filter2 = ARR_SUB([SMGUtils sortBig2Small:filter1 compareBlock:subBlock], 0, filter1.count * fuRate);
+    NSLog(@"过滤器: 总%ld需%ld 主:%.2f => 剩:%ld 辅:%.2f => 剩:%ld",protoCount,resultNum,zuRate,filter1.count,fuRate,filter2.count);
+    
+    //6. 返回结果 (参考注释公式说明-5);
     return filter2;
 }
 
