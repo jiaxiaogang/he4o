f3bdf32ccc8c84cf0f319e2cd94779fa5e458e2a
jiaxiaogang <jxg2764894@live.com>
2023-04-13 09:09:51 +0800

    override算法 (写完);

--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/TCSolution/Model/AICansetModel.m
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/TCSolution/Model/AICansetModel.m
@@ -27,27 +27,58 @@
 -(NSArray*) overrideCansets {
     //1. 数据准备;
     AIFoNodeBase *selfFo = [SMGUtils searchNode:self.scene];
-    AIFoNodeBase *baseFo = self.base ? [SMGUtils searchNode:self.base.scene] : nil;
-    NSArray *selfConCansets = [selfFo getConCansets:selfFo.count];
-    NSArray *baseConCansets = [baseFo getConCansets:baseFo.count];
+    
+    //2. 不同type的公式不同 (参考29069-todo5.3 & 5.4 & 5.5);
     if (self.type == CansetTypeBrother) {
-        //2. 当前是brother时: (brother有效canset = brother.conCansets - father.conCansets) (参考29069-todo5.3);
-        NSArray *brotherConCansets = selfConCansets;
-        NSArray *fatherConCansets = baseConCansets;
-        
-        //TODOTOMORROW20230412: 继续写完这里的mIsC过滤... (参考29069-todo5.2);
-        
-        
-        
+        //3. 当前是brother时: (brother有效canset = brother.conCansets - father.conCansets) (参考29069-todo5.3);
+        NSArray *brotherConCansets = [selfFo getConCansets:selfFo.count];
+        NSArray *fatherFilter_ps = [self getFilter_ps];
+        return [SMGUtils filterSame_ps:fatherFilter_ps parent_ps:brotherConCansets];
     } else if (self.type == CansetTypeFather) {
-        //3. 当前是father时: (father有效canset = father.conCansets - i.conCansets) (参考29069-todo5.4);
-        NSArray *fatherConCansets = selfConCansets;
-        NSArray *iConCansets = baseConCansets;
-        
-    } else if (self.type == CansetTypeFather) {
+        //4. 当前是father时: (father有效canset = father.conCansets - i.conCansets) (参考29069-todo5.4);
+        NSArray *fatherConCansets = [selfFo getConCansets:selfFo.count];
+        NSArray *iFilter_ps = [self getFilter_ps];
+        return [SMGUtils filterSame_ps:iFilter_ps parent_ps:fatherConCansets];
+    } else if (self.type == CansetTypeI) {
         //4. 当前是i时: (i有效canset = i.conCansets) (参考29069-todo5.5);
-        NSArray *iConCansets = selfConCansets;
+        NSArray *iConCansets = [selfFo getConCansets:selfFo.count];
         return iConCansets;
+    }
+    return nil;
+}
+
+/**
+ *  MARK:--------------------获取override用来过滤的部分 (参考29069-todo5.2)--------------------
+ *  @desc 取father过滤部分 (用于mIsC过滤) (参考29069-todo5.1);
+ */
+-(NSArray*) getFilter_ps {
+    //1. brother时: 取father及其具象 => 作为过滤部分 (参考29069-todo5.3-公式减数);
+    if (self.type == CansetTypeBrother) {
+        //2. 取到father的conCansets;
+        AIFoNodeBase *fatherFo = [SMGUtils searchNode:self.base.scene];
+        NSArray *fatherConCansets = [fatherFo getConCansets:fatherFo.count];
+        
+        //3. 将filter_ps收集并返回 (参考29069-todo5.3);
+        NSMutableArray *allFilter_ps = [[NSMutableArray alloc] initWithArray:fatherConCansets];
+        [allFilter_ps addObjectsFromArray:[SMGUtils convertArr:fatherConCansets convertItemArrBlock:^NSArray *(AIKVPointer *obj) {
+            AIFoNodeBase *fatherConConset = [SMGUtils searchNode:obj];
+            return Ports2Pits([AINetUtils conPorts_All:fatherConConset]);
+        }]];
+        return allFilter_ps;
+    }
+    //4. father时: 取i及其抽象 => 作为过滤部分 (参考29069-todo5.4-公式减数);
+    else if (self.type == CansetTypeFather) {
+        //5. 取到i的conCansets;
+        AIFoNodeBase *iFo = [SMGUtils searchNode:self.base.scene];
+        NSArray *iConCansets = [iFo getConCansets:iFo.count];
+        
+        //6. 将filter_ps收集并返回 (参考29069-todo5.4);
+        NSMutableArray *allFilter_ps = [[NSMutableArray alloc] initWithArray:iConCansets];
+        [allFilter_ps addObjectsFromArray:[SMGUtils convertArr:iConCansets convertItemArrBlock:^NSArray *(AIKVPointer *obj) {
+            AIFoNodeBase *iConConset = [SMGUtils searchNode:obj];
+            return Ports2Pits([AINetUtils absPorts_All:iConConset]);
+        }]];
+        return allFilter_ps;
     }
     return nil;
 }
