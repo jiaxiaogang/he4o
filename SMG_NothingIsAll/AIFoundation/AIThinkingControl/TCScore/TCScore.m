b306f8e02a5e1aad3b440abbb781eb1afb9b3624
贾晓刚 <jxg2764894@live.com>
2024-01-03 12:14:54 +0800

    继续调试31051-问题: h有皮果未执行hSolution问题;

--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/TCScore/TCScore.m
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/TCScore/TCScore.m
@@ -19,7 +19,10 @@
 +(TCResult*) scoreFromTOQueue{
     BOOL havHDemand = NUMTOOK([SMGUtils searchObjectForFilePath:kCachePath fileName:@"" time:1000]).boolValue;
     if (havHDemand) {
-        NSLog(@">>>>>>>>>>>>>>>>>>>>>>>");
+        for (DemandModel *demand in theTC.outModelManager.getAllDemand) {
+            NSLog(@"%.2f",[AIScore score4Demand:demand]);
+            NSLog(@"%@",TOModel2Sub2Str(demand));
+        }
         NSLog(@"过一秒正常,此处查下score中,为什么触发了rSolution,却没触发hSolution");
     }
     
