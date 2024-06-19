2f0a69e35bd1c4cc0f9f8b2df7975251f80a26ff
jiaxiaogang <jxg2764894@live.com>
2024-06-19 09:23:16 +0800

    写32012-TODO5: 中间帧反馈成立时outSPDic计SP+1;

--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/Model/TOFoModel.m
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/Out/Model/TOFoModel.m
@@ -339,6 +339,11 @@
     BOOL feedbackValid = [self step1_CheckFeedbackTORIsValid:feedbackMatchAlg_ps protoAlg:protoAlg_p];
     if (!feedbackValid) return false;
     
+    //2. 中间帧反馈成功时,直接计outSPDic为SP+1 (参考32012-TODO5);
+    AIFoNodeBase *sceneTo = [SMGUtils searchNode:self.sceneTo];
+    if (self.isInfected) [sceneTo updateOutSPStrong:self.cansetActIndex difStrong:-1 type:ATSub sceneFrom:self.sceneFrom cansetFrom:self.cansetFrom];//3a. 如果传染过,则先回滚一下Sub-1;
+    [sceneTo updateOutSPStrong:self.cansetActIndex difStrong:1 type:ATPlus sceneFrom:self.sceneFrom cansetFrom:self.cansetFrom];//3b. 只要反馈成功的,都进行P+1;
+    
     //2. 反馈有效: 构建hCanset;
     [self step2_FeedbackThenCreateHCanset:protoAlg_p];
         
