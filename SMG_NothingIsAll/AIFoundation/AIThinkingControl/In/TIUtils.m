97979a5d8f2477f803107349d4c549b60ef6f0ea
jiaxiaogang <jxg2764894@live.com>
2025-04-10 09:17:07 +0800

    写组特征识别算法-前期准备。

--- SMG_NothingIsAll/AIFoundation/AIThinkingControl/In/TIUtils.m
+++ SMG_NothingIsAll/AIFoundation/AIThinkingControl/In/TIUtils.m
@@ -294,7 +294,157 @@
     //64. 识别组特征。
     //递归？还是新写一个吧：毕竟现在的level,x,y和组特征的位置相符度肯定不同。
     
-    [self recognitionFeature:gtNode];
+    [self recognitionGroupFeature:gtNode];
+    
+    
+    
+    return resultModels;
+}
+
+/**
+ *  MARK:--------------------组特征识别--------------------
+ */
++(NSArray*) recognitionGroupFeature:(AIKVPointer*)protoFeature_p {
+    //1. 数据准备
+    AIFeatureNode *protoFeature = [SMGUtils searchNode:protoFeature_p];
+    NSLog(@"\n=========== 特征识别 protoT%ld（%@）===========",protoFeature_p.pointerId,protoFeature_p.dataSource);
+    AIFeatureAllBestGVModel *gvBestModel = [[AIFeatureAllBestGVModel alloc] init];
+    if (protoFeature.count == 0) return @[[[AIMatchModel alloc] initWithMatch_p:protoFeature_p]];
+    
+    //2. 循环分别识别：特征里的组码。
+    for (NSInteger i = 0; i < protoFeature.count; i++) {
+        AIKVPointer *protoGroupValue_p = ARR_INDEX(protoFeature.content_ps, i);
+        NSInteger protoLevel = NUMTOOK(ARR_INDEX(protoFeature.levels, i)).integerValue;
+        
+        //TODOTOMORROW20250410: 如果识别的是组特征，则
+        if (PitIsFeature(protoGroupValue_p)) {
+            
+            //TODOTOMORROW20250409: 从上一轮递归的识别结果assTs中的：各个absT各自再取refPort，对生成的组特征进行识别（参考34132-特征嵌套）。
+            if (PitIsFeature(refPort.target_p)) {
+                //宏观一层仍是特征：判断位置符合度，收集至结果（看复用下面的第6代码段）。
+                
+                continue;
+            }
+            
+        }
+        
+        //4. 组码识别。
+        NSArray *gMatchModels = [AIRecognitionCache getCache:protoGroupValue_p cacheBlock:^id{
+            return ARRTOOK([self recognitionGroupValueV2:protoGroupValue_p rate:0.3 minLimit:3]);
+        }];
+        
+        //6. 对所有gv识别结果的，所有refPorts，依次判断位置符合度。
+        for (AIMatchModel *gModel in gMatchModels) {
+            NSArray *refPorts = [AINetUtils refPorts_All:gModel.match_p];
+            
+            //11. 每个refPort转为model并计匹配度和匹配数;
+            for (AIPort *refPort in refPorts) {
+                
+                //12. 根据level分别记录不同deltaLevel结果（把deltaLevel做为key的一部分，记录到识别结果字典里）。
+                NSString *assKey = STRFORMAT(@"%ld_%ld",protoLevel - refPort.level,refPort.target_p.pointerId);
+                
+                //13. 取出已经收集到的assGVModels,判断下一个refPort收集进去的话,是否符合位置;
+                NSArray *assGVItems = [gvBestModel getAssGVModelsForKey:assKey];
+                //BOOL debugMode = [feature_p.dataSource isEqual:@"hColors"] && [refPort.target_p isEqual:protoFeature.p] && assLevel == protoLevel && [gModel.match_p isEqual:protoGroupValue_p];
+                CGFloat matchDegree = [ThinkingUtils checkAssToMatchDegree:protoFeature protoIndex:i assGVModels:assGVItems checkRefPort:refPort debugMode:false];
+                
+                //14. 判断新一条refPort是否更好，更好的话存下来（存refPort，assKey，gModel.matchValue，matchDegree）。
+                [gvBestModel updateStep1:assKey refPort:refPort gMatchValue:gModel.matchValue gMatchDegree:matchDegree matchOfProtoIndex:i];
+                
+                
+                
+                //TODOTOMORROW20250410: 把识别到的refPort在protoFeature中的位置生成rect。
+            }
+        }
+        
+        //21. STEP2：每个protoIndex内防重，竞争只保留protoIndex下最好一条。
+        [gvBestModel invokeRankStep2];
+        
+        //22. STEP3：跨protoIndex防重，将best结果存下来
+        [gvBestModel updateStep3];
+    }
+    //31. 用明细生成总账（bestModel -> resultDic）。
+    NSDictionary *resultDic = [gvBestModel convert2AIMatchModelsStep4];// <K=deltaLevel_assPId, V=识别的特征AIMatchModel>
+    
+    //32. debug
+    for (NSString *assKey in resultDic.allKeys) {
+        AIMatchModel *model = [resultDic objectForKey:assKey];
+        if (Log4RecogDesc) NSLog(@"%@\t匹配条数 %ld/%ld \t特征识别综合匹配度计算:T%ld \t匹配度:%.2f / %ld \t= %.2f 总强度：%ld 均强度：%.1f",assKey,model.matchCount,protoFeature.count,model.match_p.pointerId,model.sumMatchValue,model.matchCount,model.matchValue,model.sumRefStrong,model.strongValue);
+    }
+    
+    //41. 无效过滤器1、matchValue=0排除掉。
+    NSArray *resultModels = [SMGUtils filterArr:resultDic.allValues checkValid:^BOOL(AIMatchModel *item) {
+        return item.matchValue > 0;
+    }];
+    
+    //42. 防重过滤器2、此处每个特征的不同层级，可能识别到同一个特征，可以按匹配度防下重。
+    resultModels = [SMGUtils removeRepeat:[SMGUtils sortBig2Small:resultModels compareBlock:^double(AIMatchModel *obj) {
+        return obj.matchCount;
+    }] convertBlock:^id(AIMatchModel *obj) {
+        return obj.match_p;
+    }];
+    
+    //43. 末尾淘汰20%被引用强度最低的。
+    resultModels = ARR_SUB([SMGUtils sortBig2Small:resultModels compareBlock:^double(AIMatchModel *obj) {
+        return obj.strongValue;
+    }], 0, MAX(resultModels.count * 0.8f, 10));
+    
+    //44. 末尾淘汰仅保留匹配数大于xx%的：全含判断=>特征应该不需要全含，因为很难看到局部都相似的两个图像。
+    resultModels = [SMGUtils filterArr:resultModels checkValid:^BOOL(AIMatchModel *item) {
+        AIFeatureNode *tNode = [SMGUtils searchNode:item.match_p];
+        return item.matchCount > tNode.count * 0.3;
+    }];
+    
+    //45. 末尾淘汰匹配数小于3条的、组码太少，形不成什么显著的特征。
+    //2025.04.07: 由绝对3条淘汰改成末尾淘汰：匹配数低的占比偏多，所以改成按匹配数排序尾部淘汰。
+    //2025.04.08: 由末尾淘汰改成平均匹配数淘汰：BUG-修复最后很多1号坚果都是GV匹配数=2的，但H通道很重要，改成以matchCount的和来判断匹配数（经实测已OK)。
+    NSInteger pinJunMatchCount = [SMGUtils sumOfArr:resultModels convertBlock:^double(AIMatchModel *obj) {
+        return obj.matchCount;
+    }] / (float)resultModels.count;
+    resultModels = [SMGUtils filterArr:resultModels checkValid:^BOOL(AIMatchModel *item) {
+        return item.matchCount >= pinJunMatchCount;
+    }];
+    
+    //46. 末尾淘汰xx%匹配度低的、匹配度强度过滤器 (参考28109-todo2 & 34091-5提升准确)。
+    resultModels = ARR_SUB([SMGUtils sortBig2Small:resultModels compareBlock:^double(AIMatchModel *obj) {
+        return obj.matchValue * obj.matchDegree;
+    }], 0, MIN(MAX(resultModels.count * 0.7f, 10), 20));
+    
+    //51. 更新: ref强度 & 相似度 & 抽具象 & 映射;
+    for (AIMatchModel *matchModel in resultModels) {
+        AIFeatureNode *assFeature = [SMGUtils searchNode:matchModel.match_p];
+        [AINetUtils insertRefPorts_General:assFeature.p content_ps:assFeature.content_ps difStrong:1 header:assFeature.header];
+        [protoFeature updateMatchValue:assFeature matchValue:matchModel.matchValue];
+        [AINetUtils relateGeneralAbs:assFeature absConPorts:assFeature.conPorts conNodes:@[protoFeature] isNew:false difStrong:1];
+        [protoFeature updateIndexDic:assFeature indexDic:matchModel.indexDic];
+        [protoFeature updateDegreeDic:assFeature.pId degreeDic:matchModel.degreeDic];
+        
+        //52. debug
+        if (Log4RecogDesc || true) NSLog(@"特征识别结果:T%ld%@\t 匹配条数:%ld/(proto%ld ass%ld)\t匹配度:%.2f\t强度:%.1f\t符合度:%.1f",
+                                         matchModel.match_p.pointerId,CLEANSTR([assFeature getLogDesc:true]),matchModel.matchCount,protoFeature.count,assFeature.count,matchModel.matchValue,matchModel.strongValue,matchModel.matchDegree);
+    }
+    
+    
+    //TODOTOMORROW20250410: 把同时识别到的assTs中，位置符合的，打包成一组特征，然后递归至宏观一层继续特征识别。
+    
+    //61. 将每个matchModel转成rect（根据matchModel的indexDic可以取得protoIndexes，然后根据这个下标组来取并集区域范围）。
+    NSArray *rects = [SMGUtils convertArr:resultModels convertBlock:^id(AIMatchModel *model) {
+        CGRect modelRect = [AINetUtils convertPartOfFeatureContent2Rect:protoFeature contentIndexes:model.indexDic.allValues];
+        return @(modelRect);
+    }];
+    
+    //62. 将matchModels转成组特征的content_ps。
+    NSArray *gtContent_ps = [SMGUtils convertArr:resultModels convertBlock:^id(AIMatchModel *obj) {
+        return obj.match_p;
+    }];
+    
+    //63. 构建组特征。
+    AIFeatureNode *gtNode = [AIGeneralNodeCreater createFeatureNode:gtContent_ps conNodes:nil at:protoFeature.p.algsType ds:protoFeature.p.dataSource isOut:protoFeature.p.isOut];
+    
+    //64. 识别组特征。
+    //递归？还是新写一个吧：毕竟现在的level,x,y和组特征的位置相符度肯定不同。
+    
+    [self recognitionGroupFeature:gtNode];
     
     
     
